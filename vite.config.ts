da6bd7a766e2c5ac2a23d14d7c78457ee64d2f24
Aaron Cox <aaron@greymass.com>
2024-07-15 17:59:17 -0700

    Compile ParaglideJS on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'node:path';
 
 export default defineConfig({
-	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }), sveltekit()]
+	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }), sveltekit()],
+	resolve: {
+		alias: {
+			$paraglide: path.resolve(__dirname, './src/paraglide')
+		}
+	}
 });
