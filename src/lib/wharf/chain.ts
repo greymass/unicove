244d428ce1df34fe69b651c99aaca1ed41dc28b2
Aaron Cox <aaron@greymass.com>
2024-07-15 18:01:03 -0700

    Remove environmental variable controlling chain

--- src/lib/wharf/chain.ts
+++ src/lib/wharf/chain.ts
@@ -1,8 +1,3 @@
 import { Chains } from '@wharfkit/common';
-import { PUBLIC_CHAIN_NAME } from '$env/static/public';
 
-if (!Chains[PUBLIC_CHAIN_NAME]) {
-	throw new Error(`Unknown chain: ${PUBLIC_CHAIN_NAME}`);
-}
-
-export const chain = Chains[PUBLIC_CHAIN_NAME];
+export const chain = Chains.Jungle4;
