0517c950c6d49d304d21f368511a1efe495e19b5
Aaron Cox <aaron@greymass.com>
2024-07-15 17:31:52 -0700

    Conditional check (needs rework)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,20 @@
 </script>
 
 <h3>Active Session</h3>
-{#if user.session}
-	<p>Current: {user.session.actor}</p>
-	<button onclick={() => user.logout(user.session)}>Logout current account</button>
-	<button onclick={() => user.logout()}>Logout all accounts</button>
-{/if}
+{#if user}
+	{#if user.session}
+		<p>Current: {user.session.actor}</p>
+		<button onclick={() => user.logout(user.session)}>Logout current account</button>
+		<button onclick={() => user.logout()}>Logout all accounts</button>
+	{/if}
 
-<h3>Sessions</h3>
-<button onclick={() => user.login()}>Login</button>
-{#each user.sessions as session}
-	<p>
-		<button onclick={() => user.switch(session)}>
-			Switch: {session.actor}@{session.permission} ({session.chain})
-		</button>
-	</p>
-{/each}
+	<h3>Sessions</h3>
+	<button onclick={() => user.login()}>Login</button>
+	{#each user.sessions as session}
+		<p>
+			<button onclick={() => user.switch(session)}>
+				Switch: {session.actor}@{session.permission} ({session.chain})
+			</button>
+		</p>
+	{/each}
+{/if}
