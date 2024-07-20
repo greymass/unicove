a9afa7106002c3177990f5222dc2b862516999de
Aaron Cox <aaron@greymass.com>
2024-07-19 18:43:14 -0700

    Modify canonical url to be base path without language

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,11 @@
+import { i18n } from '$lib/i18n';
 import { type SeoConfig } from 'svead';
 
 export const load = ({ url }) => {
+	const modified = new URL(url);
+	modified.pathname = i18n.route(url.pathname);
 	const baseMetaTags: SeoConfig = {
-		url: String(url),
+		url: String(modified),
 		title: '2nicove',
 		description: 'Unicove, but 2.0'
 	};
