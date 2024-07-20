7b49261c7e0d406b8e8991c21d4b744be9eafd5b
Aaron Cox <aaron@greymass.com>
2024-07-20 13:23:44 -0700

    Fixed imports

--- src/routes/(explorer)/msig/[proposer]/[proposal]/+page.svelte
+++ src/routes/(explorer)/msig/[proposer]/[proposal]/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import type { PageData } from '../$types';
+	import type { PageData } from './$types';
 	import * as m from '$lib/paraglide/messages.js';
 
 	export let data: PageData;
 </script>
 
-<h1>{data.id}</h1>
+<h1>{data.proposer}/{data.proposer}</h1>
 
 <pre>
 {JSON.stringify(data.proposal, null, 2)}
