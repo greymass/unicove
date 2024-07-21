79475a0b1eb1d1648b94ed5b55b1e3aedefd61da
Aaron Cox <aaron@greymass.com>
2024-07-20 17:21:33 -0700

    Bringing in account/contract kits, and syncing account

--- src/lib/wharf/user.svelte.ts
+++ src/lib/wharf/user.svelte.ts
@@ -6,6 +6,7 @@ import {
 	type RestoreArgs,
 	type SerializedSession,
 	type WalletPlugin,
+	APIClient,
 	Chains,
 	Session,
 	SessionKit
@@ -14,6 +15,10 @@ import WebRenderer from '@wharfkit/web-renderer';
 
 import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor';
 import { WalletPluginPrivateKey } from '@wharfkit/wallet-plugin-privatekey';
+import { Account, AccountKit } from '@wharfkit/account';
+import ContractKit, { Contract } from '@wharfkit/contract';
+import { Contract as TokenContract } from './contracts/token';
+import { Contract as SystemContract } from './contracts/system';
 
 const walletPlugins: WalletPlugin[] = [new WalletPluginAnchor()];
 
@@ -22,7 +27,16 @@ if (PUBLIC_LOCAL_SIGNER) {
 	walletPlugins.unshift(new WalletPluginPrivateKey(PUBLIC_LOCAL_SIGNER));
 }
 
+interface DefaultContracts {
+	token?: TokenContract;
+	system?: SystemContract;
+}
+
 export class User {
+	public account: Account | undefined = $state<Account | undefined>();
+	public accountKit: AccountKit | undefined = $state<AccountKit | undefined>();
+	public contracts: DefaultContracts = $state({});
+	public contractKit: ContractKit | undefined = $state<ContractKit | undefined>();
 	public session: Session | undefined = $state<Session | undefined>();
 	public sessions: SerializedSession[] = $state([]);
 	public sessionKit: SessionKit | undefined;
@@ -31,12 +45,34 @@ export class User {
 		if (!browser) {
 			throw new Error('User should only be used in the browser');
 		}
+		const chain = Chains.Jungle4;
+		const client = new APIClient({ url: chain.url });
+		this.accountKit = new AccountKit(chain, {
+			client
+		});
+		this.contractKit = new ContractKit({
+			client
+		});
+
+		this.contracts.token = new TokenContract({ client });
+		this.contracts.system = new SystemContract({ client });
+
 		this.sessionKit = new SessionKit({
 			appName: 'unicove',
-			chains: [Chains.Jungle4],
+			chains: [chain],
 			ui: new WebRenderer({ minimal: true }),
 			walletPlugins
 		});
+	}
+
+	private async loadAccount() {
+		if (!this.accountKit) {
+			throw new Error('AccountKit not initialized');
+		}
+		if (!this.session) {
+			throw new Error('Session not initialized');
+		}
+		this.account = await this.accountKit.load(this.session.actor);
 	}
 
 	public async login(options?: LoginOptions) {
@@ -46,6 +82,7 @@ export class User {
 		const result = await this.sessionKit.login(options);
 		this.session = result.session;
 		this.sessions = await this.sessionKit.getSessions();
+		await this.loadAccount();
 	}
 
 	public async logout(session?: Session | SerializedSession) {
@@ -68,6 +105,7 @@ export class User {
 		const restored = await this.sessionKit.restore(args, options);
 		if (restored) {
 			this.session = restored;
+			await this.loadAccount();
 		}
 		this.sessions = await this.sessionKit.getSessions();
 		return restored;
