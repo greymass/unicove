357160d677923f7aea4480ad3201084db21491b3
Aaron Cox <aaron@greymass.com>
2024-07-21 18:44:04 -0700

    Use auto adapter by default

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
