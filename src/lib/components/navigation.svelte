14a1187544a5f3a11450a0d62f2dde47270d4bd7
Aaron Cox <aaron@greymass.com>
2024-07-21 18:43:37 -0700

    Added example links

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -1,2 +1,8 @@
 <a href="/">Home</a>
-<a href="/account">Account</a>
+<a href="/account">My Account</a>
+<a href="/account/shipload.gm">Account: shipload.gm</a>
+<a href="/block/100000">Block 100,000</a>
+<a href="/transaction/f45c450512181315e811f3a4f29a2ac64c96fdbb3528de97a8907f80abfe74c8"
+	>Transaction</a
+>
+<a href="/msig/evilmikehere/t">msig</a>
