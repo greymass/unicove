3391e67f228394a0e8cd9119c65d655f46327930
Aaron Cox <aaron@greymass.com>
2024-07-23 12:38:19 -0700

    Split transactions into multiple categories

--- src/lib/components/transactions.svelte
+++ src/lib/components/transactions.svelte
@@ -1,26 +1,43 @@
 <script lang="ts">
 	import { StatusType, transactions } from '$lib/wharf/transact.svelte';
 
-	const pending = $derived(transactions.filter((t) => t.status === StatusType.BROADCAST));
+	const pendingStatuses = [StatusType.BROADCAST, StatusType.IN_BLOCK, StatusType.LOCALLY_APPLIED];
+	const pending = $derived(transactions.filter((t) => pendingStatuses.includes(t.status)));
+
+	const completeStatuses = [StatusType.IRREVERSIBLE, StatusType.ERROR];
+	const complete = $derived(transactions.filter((t) => completeStatuses.includes(t.status)));
 </script>
 
-<h1>All</h1>
+<h1>Pending Transactions</h1>
 
 <table border="1">
 	<tbody>
-		{#each transactions as transaction}
+		{#each pending as transaction}
 			<tr>
 				<td>{transaction.status}</td>
-				<td><pre>{transaction.transaction.id}</pre></td>
+				{#if transaction.transaction}
+					<td><pre>{transaction.transaction.id}</pre></td>
+				{:else}
+					<td>{transaction.error}</td>
+				{/if}
 			</tr>
 		{/each}
 	</tbody>
 </table>
 
-<h1>Pending Transactions</h1>
+<h1>Completed Transactions</h1>
 
-{#each pending as transaction}
-	<div>
-		<pre>{transaction.transaction.id}</pre>
-	</div>
-{/each}
+<table border="1">
+	<tbody>
+		{#each complete as transaction}
+			<tr>
+				<td>{transaction.status}</td>
+				{#if transaction.transaction}
+					<td><pre>{transaction.transaction.id}</pre></td>
+				{:else}
+					<td>{transaction.error}</td>
+				{/if}
+			</tr>
+		{/each}
+	</tbody>
+</table>
