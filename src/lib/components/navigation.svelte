2cdbea7f2e5b83fede760875084eaf315b74bb1b
Aaron Cox <aaron@greymass.com>
2024-07-23 16:23:20 -0700

    Implemented multi-network

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -1,6 +1,6 @@
 <a href="/">Home</a>
 <a href="/account">My Account</a>
-<a href="/account/shipload.gm">Account: shipload.gm</a>
+<a href="/account/eosio">Account: eosio</a>
 <a href="/block/100000">Block 100,000</a>
 <a href="/transaction/f45c450512181315e811f3a4f29a2ac64c96fdbb3528de97a8907f80abfe74c8"
 	>Transaction</a
