636aa95afd6f703d5fb87fb465b8c0e3ee04a2e7
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-07-25 15:45:41 -0700

    wip

--- src/lib/components/three/scene.svelte
+++ src/lib/components/three/scene.svelte
@@ -3,22 +3,54 @@ import { T, useTask, useLoader } from '@threlte/core'
 import { interactivity } from '@threlte/extras'
 
 import Logo from './assets/unicove_logo_3d.svelte'
-import Duck from './assets/Duck.svelte'
 
-let rotation = 0
+// let rotation = 0
 
 // useTask is relying on a context provided
 // by <Canvas>. Because we are definitely *inside*
 // <Canvas>, we can safely use it.
-useTask((delta) => {
-	rotation += delta
-})
+// useTask((delta) => {
+// 	rotation += delta
+// })
 
 // This file is also typically the place to
 // inject plugins
 interactivity()
+
+let mouseX = $state(0)
+let mouseY = $state(0)
+
+let windowWidth
+let windowHeight
+
+function handleMouseMove(e) {
+	mouseX = e.clientX
+	mouseY = e.clientY
+}
+
+let canvasWidth = 10
+let canvasHeight = 10
+
+let lightX = $derived(((mouseX / windowWidth) * canvasWidth) - (canvasWidth / 2))
+let lightY = $derived(-(((mouseY / windowHeight) * canvasHeight) - (canvasHeight / 2)))
+
+// $inspect({ lightX, lightY })
+
 </script>
 
-<Logo scale={2} rotation.x={Math.PI /2} rotation.z={rotation}/>
+<svelte:window on:mousemove={handleMouseMove} 
+	bind:innerWidth={windowWidth}
+	bind:innerHeight={windowHeight} />
+
+<!-- <T.AxesHelper size={10} /> -->
+
+<!-- <T.Mesh position.x={lightX} position.y={lightY} position.z={1}> -->
+<!-- 	<T.BoxGeometry args={[1, 1, 1]} /> -->
+<!-- 	<T.MeshStandardMaterial color="red" /> -->
+<!-- </T.Mesh> -->
+
+<T.DirectionalLight position={[lightX, lightY, 1]} />
+
+<Logo scale={4}  rotation.x={Math.PI /2} />
 
 
