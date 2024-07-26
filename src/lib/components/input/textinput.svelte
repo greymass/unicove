f5c66835ad1f19e592c9d53dccb89fd4cb4bc64d
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-07-26 11:19:03 -0700

    Add label component

--- src/lib/components/input/textinput.svelte
+++ src/lib/components/input/textinput.svelte
@@ -15,4 +15,4 @@ let {value = $bindable(), ...props} : Props = $props();
 
 </script>
 
-<input  class='py-4 px-4 rounded-lg bg-transparent outline outline-1 outline-gray-700 focus:outline-none focus:ring focus:ring-inset focus:ring-blue-500' type="text" value={value} {...props}/>
+<input class='py-4 px-4 rounded-lg bg-transparent outline outline-1 outline-gray-700 focus:outline-none focus:ring focus:ring-inset focus:ring-blue-500' type="text" value={value} {...props}/>
