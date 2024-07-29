f0c27d9ba9aa9ed31d18a60ffbf73e8ba7ad2d95
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-07-29 13:23:43 -0700

    More button style updates

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -3,6 +3,7 @@ interface Props {
 	href?: string;
 	variant?: 'primary' | 'secondary' | 'pill';
 	disabled?: boolean;
+	active?: boolean;
 }
 
 let props: Props = $props();
@@ -11,13 +12,13 @@ let props: Props = $props();
 {#if props.variant === 'pill'}
 	<svelte:element
 		this={props.href ? 'a' : 'button'}
-		class={ `py-2.5 px-8 font-medium rounded-full relative \
+		class={ `inline-block m-0.5 py-2.5 px-6 font-medium rounded-full relative leading-4 \
 	before:absolute before:opacity-0 before:rounded-full before:inset-0 before:transition-opacity \
   before:bg-mineShaft-900 [@media(any-hover:hover){&:hover}]:before:opacity-100 [@media(any-hover:hover){&:hover}]:text-white \
 	after:absolute after:opacity-0  after:rounded-full after:inset-0 after:transition-opacity after:duration-100 after:delay-0 \
   after:bg-mineShaft-950 active:after:opacity-100 active:text-white \
-	focus-visible:outline focus:outline-3 focus:outline-solar-500 focus-visible:before:opacity-100 focus-visible:before:transition-none \
-target:outline target:outline-1 target:outline-mineShaft-950` }
+	focus-visible:outline focus-visible:outline-2 focus-visible:outline-solar-500 focus-visible:before:opacity-100 focus-visible:before:transition-none \
+	target:outline target:outline-2 target:outline-mineShaft-200/30` }
 		{...props}
 	>
 		<span class="relative z-10">{@render props.children()}</span>
@@ -26,12 +27,12 @@ target:outline target:outline-1 target:outline-mineShaft-950` }
 {:else if props.variant === 'secondary'}
 	<svelte:element
 		this={props.href ? 'a' : 'button'}
-		class={ `py-3 px-8  relative font-medium rounded-lg  border-2 border-mineShaft-600 \
+		class={ `m-0.5 py-3 px-8  relative font-medium rounded-lg  border-2 border-mineShaft-600 \
 	before:absolute before:opacity-0  before:rounded-lg before:-inset-0.5 before:transition-opacity \
   before:bg-mineShaft-900 [@media(any-hover:hover){&:hover}]:before:opacity-100 [@media(any-hover:hover){&:hover}]:text-white \
 	after:absolute after:opacity-0  after:rounded-lg after:-inset-0.5 after:transition-opacity after:duration-100 after:delay-0 \
   after:bg-mineShaft-950 active:after:opacity-100 active:text-white \
-	focus-visible:outline focus:outline-3 focus:outline-solar-500 focus-visible:before:opacity-100 focus-visible:before:transition-none 
+	focus-visible:outline focus:outline-2 focus:outline-solar-500 focus-visible:before:opacity-100 focus-visible:before:transition-none 
 ` }
 		{...props}
 	>
@@ -41,12 +42,12 @@ target:outline target:outline-1 target:outline-mineShaft-950` }
 {:else}
 	<svelte:element
 		this={props.href ? 'a' : 'button'}
-		class={ `py-3.5 px-8 text-white relative font-medium rounded-lg relative bg-pictonBlue-500 \
+		class={ `m-0.5 py-3.5 px-8 text-white relative font-medium rounded-lg relative bg-pictonBlue-500 \
 	before:absolute before:opacity-0  before:rounded-lg before:inset-0 before:transition-opacity \
   before:bg-pictonBlue-400 [@media(any-hover:hover){&:hover}]:before:opacity-100 \
 	after:absolute after:opacity-0  after:rounded-lg after:inset-0 after:transition-opacity after:duration-100 after:delay-0 \
   after:bg-pictonBlue-600 active:after:opacity-100 active:text-white \
-	focus-visible:outline focus:outline-3 focus:outline-solar-500
+	focus-visible:outline focus:outline-2 focus:outline-solar-500
 	` }
 		{...props}
 	>
