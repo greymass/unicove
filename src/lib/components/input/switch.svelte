990866926c6589729ba4a1f80330a2af8e6ec408
Guras <sandro.guras@pm.me>
2024-07-30 17:43:12 -0400

    Switch element
    * styled
    * add interface

--- src/lib/components/input/switch.svelte
+++ src/lib/components/input/switch.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
+	interface Props {
+		disabled?: boolean;
+		label?: string;
+		checked?: boolean;
+	}
 	import { createSwitch, melt } from '@melt-ui/svelte';
 	import Label from './label.svelte';
 
 	const {
-		elements: { root, input },
-	} = createSwitch();
+		elements: { root, input, options },
+	} = createSwitch({ disabled: false });
+
+	let props: Props = $props();
 </script>
 
 <div class="flex items-center">
 	<Label class="pr-4">Airplane mode</Label>
 	<button
 		use:melt={$root}
-		class="relative outline outline-offset-1 outline-gray-700 h-4/5 ml-5 cursor-default rounded-full bg-black transition-colors data-[state=checked]:bg-magnum-950"
+		class="relative outline outline-2 outline-offset-1 outline-gray-700 h-4/5 ml-5 cursor-default rounded-full bg-zinc-900 transition-colors data-[state=checked]:bg-pictonBlue-500 focus-visible:outline-yellow-500 data-[state=checked]:outline-pictonBlue-500 data-[state=checked]:outline-offset-0 data-[state=checked]:hover:bg-sky-400 data-[state=checked]:hover:outline-sky-400 data-[state=checked]:focus-visible:outline-yellow-500"
 		id="airplane-mode"
 		aria-labelledby="airplane-mode-label"
 	>
-		<span class="thumb block rounded-full bg-mineShaft-300 transition" />
+		<span class="thumb block rounded-full bg-neutral-400 transition" />
 	</button>
 	<input use:melt={$input} />
 </div>
@@ -36,5 +43,6 @@
 
     :global([data-state='checked']) .thumb {
         transform: translateX(calc(var(--w) - var(--size) - var(--padding)));
+        background-color: white;
     }
 </style>
