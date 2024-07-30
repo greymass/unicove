ee954d872db5c68c31ab9a8e029667a258bdf5bb
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-07-30 16:25:34 -0700

    Fix centering behavior

--- src/routes/(dev)/debug/components/+page.svelte
+++ src/routes/(dev)/debug/components/+page.svelte
@@ -127,41 +127,39 @@
 			<Stack id="cards">
 				<h2 class="h2">Cards</h2>
 
-				<Cluster class='justify-center items-start'>
-					<Card class='gap-5'>
-						<Stack class='gap-0'>
-							<p class='caption'>Currently Staked - 34% APY</p>
-							<p class='h3'>0.0 EOS</p>
-							<p class='bg-shark-800/60 rounded self-start px-2 mt-1.5'>USD Value $0.0</p>
-						</Stack>
-						<!-- <Switcher threshold='30rem' > -->
-						<Grid itemWidth='8ch'>
-							<Button variant="secondary" class="text-skyBlue-500">Stake</Button>
-							<Button variant="secondary" class="text-skyBlue-500">Unstake</Button>
-						</Grid>
-						<!-- </Switcher> -->
-					</Card>
+				<div class='flex justify-center'>
+					<Switcher threshold='40rem' class='items-start'>
+						<Card class='gap-5'>
+							<Stack class='gap-0'>
+								<p class='caption'>Currently Staked - 34% APY</p>
+								<p class='h3'>0.0 EOS</p>
+								<p class='bg-shark-800/60 rounded self-start px-2 mt-1.5'>USD Value $0.0</p>
+							</Stack>
+							<Switcher threshold='20rem'>
+								<Button variant="secondary" class="text-skyBlue-500">Stake</Button>
+								<Button variant="secondary" class="text-skyBlue-500">Unstake</Button>
+							</Switcher>
+						</Card>
 
-					<Card title="Unstaking Balances">
-						<table class='table-auto'>
-							<thead class='border-b-2 border-shark-100/10'>
-								<tr class='caption font-medium'>
-									<th class='p-4 text-left'>Amount</th>
-									<th class='p-4 text-right'>Date available</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td class='p-4'>0 EOS</td>
-									<td class='p-4 text-right'>Not connected</td>
-								</tr>
-							</tbody>
-						</table>
-						<Button variant="secondary" class="text-skyBlue-500">Withdraw</Button>
-					</Card>
-				</Cluster>
-
-
+						<Card title="Unstaking Balances Is A Long Title">
+							<table class='table-auto'>
+								<thead class='border-b-2 border-shark-100/10'>
+									<tr class='caption font-medium'>
+										<th class='p-4 text-left'>Amount</th>
+										<th class='p-4 text-right'>Date available</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td class='p-4'>0 EOS</td>
+										<td class='p-4 text-right'>Not connected</td>
+									</tr>
+								</tbody>
+							</table>
+							<Button variant="secondary" class="text-skyBlue-500">Withdraw</Button>
+						</Card>
+					</Switcher>
+				</div>
 			</Stack>
 
 			<hr class="h-px my-8 bg-slate-200 border-0 dark:bg-slate-800" />
@@ -243,7 +241,7 @@
 				<Stack>
 					<h3 class="h3">Center</h3>
 					<Center>
-						<Card>
+						<Card class='box-content max-w-prose'>
 							<Stack>
 								<p>
 									Switches the context to content-box and centers the element. No padding by default
