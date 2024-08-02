e00b30141fa234a850617c6e7895e16d984d54cc
Aaron Cox <aaron@greymass.com>
2024-08-02 14:25:54 -0700

    Conditionally display based on session

--- src/routes/(dev)/debug/[[network]]/state/account/+page.svelte
+++ src/routes/(dev)/debug/[[network]]/state/account/+page.svelte
@@ -13,8 +13,12 @@
 <div class="p-8 space-y-4">
 	<h2 class="h2">Account State</h2>
 	<p>The account state for the currently active session.</p>
-	<Button onclick={() => account.refresh()}>Update account state</Button>
-	<Code>
-		{JSON.stringify(account, null, 2)}
-	</Code>
+	{#if account.name}
+		<Button onclick={() => account.refresh()}>Update account state</Button>
+		<Code>
+			{JSON.stringify(account, null, 2)}
+		</Code>
+	{:else}
+		<p>No account state available.</p>
+	{/if}
 </div>
