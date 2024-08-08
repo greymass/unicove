34b48d83fee2bcc6f4a9f1b08c229678626f8b88
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-08-08 11:52:07 -0700

    Add disabled styles to buttons

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -13,22 +13,55 @@
 		children: Snippet;
 	}
 
-	let { class: className, ...props }: ButtonProps = $props();
+	let { class: className = '', ...props }: ButtonProps = $props();
 </script>
 
 {#if props.variant === 'pill'}
 	<svelte:element
 		this={props.href ? 'a' : 'button'}
 		data-active={props.active}
-		class={`inline-block text-nowrap m-0.5 py-2.5 px-6 font-medium rounded-full relative leading-4 \
-	before:absolute before:opacity-0 before:rounded-full before:-inset-0.5 before:transition-opacity \
-  before:bg-mineShaft-900 [@media(any-hover:hover){&:hover}]:before:opacity-100 [@media(any-hover:hover){&:hover}]:text-white \
-	after:absolute after:opacity-0  after:rounded-full after:inset-0 after:transition-opacity after:duration-100 after:delay-0 \
-  after:bg-mineShaft-950 active:after:opacity-100 active:text-white \
-	focus-visible:outline focus-visible:outline-2 focus-visible:outline-solar-500 focus-visible:before:inset-0 focus-visible:before:opacity-100 focus-visible:before:transition-none \
-data-[active=true]:ring data-[active=true]:ring-2 data-[active=true]:ring-mineShaft-200/30 \
-data-[active=true]:before:ring data-[active=true]:before:ring-inset data-[active=true]:before:ring-2 data-[active=true]:before:ring-mineShaft-200/30 \
-	${className}`}
+		class="
+		relative
+		m-0.5
+		inline-block
+		text-nowrap
+		rounded-full
+		px-6
+		py-2.5
+		font-medium
+		leading-4
+		before:absolute
+		before:-inset-0.5
+		before:rounded-full
+		before:bg-mineShaft-900
+		before:opacity-0
+		before:transition-opacity
+		after:absolute
+		after:inset-0
+		after:rounded-full
+		after:bg-mineShaft-950
+		after:opacity-0
+		after:transition-opacity
+		after:delay-0
+		after:duration-100
+		focus-visible:outline
+		focus-visible:outline-2
+		focus-visible:outline-solar-500
+		focus-visible:before:inset-0
+		focus-visible:before:opacity-100
+		focus-visible:before:transition-none
+		active:text-white
+		active:after:opacity-100
+		data-[active=true]:ring
+		data-[active=true]:ring-2
+		data-[active=true]:ring-mineShaft-200/30
+		data-[active=true]:before:ring
+		data-[active=true]:before:ring-2
+		data-[active=true]:before:ring-inset
+		data-[active=true]:before:ring-mineShaft-200/30
+		[@media(any-hover:hover)]:hover:text-white
+		[@media(any-hover:hover)]:hover:before:opacity-100
+		{className}"
 		{...props}
 	>
 		<span class="relative z-10">{@render props.children()}</span>
@@ -36,14 +69,47 @@ data-[active=true]:before:ring data-[active=true]:before:ring-inset data-[active
 {:else if props.variant === 'secondary'}
 	<svelte:element
 		this={props.href ? 'a' : 'button'}
-		class={`m-0.5 py-3 px-8 text-nowrap  relative font-medium rounded-lg  border-2 border-mineShaft-600 \
-	before:absolute before:opacity-0  before:rounded-lg before:-inset-0.5 before:transition-opacity \
-  before:bg-mineShaft-900 [@media(any-hover:hover){&:hover}]:before:opacity-100 [@media(any-hover:hover){&:hover}]:text-white \
-	after:absolute after:opacity-0  after:rounded-lg after:-inset-0.5 after:transition-opacity after:duration-100 after:delay-0 \
-  after:bg-mineShaft-950 active:after:opacity-100 active:text-white \
-	focus-visible:outline focus:outline-2 focus:outline-solar-500 focus-visible:before:opacity-100 focus-visible:before:transition-none \
-	${className}
-`}
+		disabled={props.disabled}
+		class="
+		relative
+		m-0.5
+		text-nowrap
+		rounded-lg
+		border-2
+		border-mineShaft-600
+		px-8
+		py-3
+		font-medium
+		before:absolute
+		before:-inset-0.5
+		before:rounded-lg
+		before:bg-mineShaft-900
+		before:opacity-0
+		before:transition-opacity
+		after:absolute
+		after:-inset-0.5
+		after:rounded-lg
+		after:bg-mineShaft-950
+		after:opacity-0
+		after:transition-opacity
+		after:delay-0
+		after:duration-100
+		focus:outline-2
+		focus:outline-solar-500
+		focus-visible:outline
+		focus-visible:before:opacity-100
+		focus-visible:before:transition-none
+		active:text-white
+		active:after:opacity-100
+		disabled:cursor-not-allowed
+		disabled:text-white/40
+		disabled:before:hidden
+		disabled:after:hidden
+		disabled:hover:text-white/40
+		[@media(any-hover:hover)]:hover:text-white
+		[@media(any-hover:hover)]:hover:before:opacity-100
+		{className}
+		"
 		{...props}
 	>
 		<span class="relative z-10">{@render props.children()}</span>
@@ -51,14 +117,44 @@ data-[active=true]:before:ring data-[active=true]:before:ring-inset data-[active
 {:else}
 	<svelte:element
 		this={props.href ? 'a' : 'button'}
-		class={`m-0.5 py-3.5 px-8 text-white text-nowrap relative font-medium rounded-lg relative bg-skyBlue-500 \
-	before:absolute before:opacity-0  before:rounded-lg before:inset-0 before:transition-opacity \
-  before:bg-skyBlue-400 [@media(any-hover:hover){&:hover}]:before:opacity-100 \
-	after:absolute after:opacity-0  after:rounded-lg after:inset-0 after:transition-opacity after:duration-100 after:delay-0 \
-  after:bg-skyBlue-600 active:after:opacity-100 active:text-white \
-	focus-visible:outline focus:outline-2 focus:outline-solar-500 \
-	${className}
-	`}
+		disabled={props.disabled}
+		class="
+		relative
+		m-0.5
+		text-nowrap
+		rounded-lg
+		bg-skyBlue-500
+		px-8
+		py-3.5
+		font-medium
+		text-white
+		before:absolute
+		before:inset-0
+		before:rounded-lg
+		before:bg-skyBlue-400
+		before:opacity-0
+		before:transition-opacity
+		after:absolute
+		after:inset-0
+		after:rounded-lg
+		after:bg-skyBlue-600
+		after:opacity-0
+		after:transition-opacity
+		after:delay-0
+		after:duration-100
+		focus:outline-2
+		focus:outline-solar-500
+		focus-visible:outline
+		active:text-white
+		active:after:opacity-100
+		disabled:cursor-not-allowed
+		disabled:bg-mineShaft-900
+		disabled:text-white/40
+		disabled:before:hidden
+		disabled:after:hidden
+		[@media(any-hover:hover)]:hover:before:opacity-100
+		{className}
+		"
 		{...props}
 	>
 		<span class="relative z-10">{@render props.children()}</span>
--- src/routes/(dev)/debug/components/sections/buttons.svelte
+++ src/routes/(dev)/debug/components/sections/buttons.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Stack } from '$lib/components/layout';
+	import { Cluster, Stack } from '$lib/components/layout';
 	import Button from '$lib/components/button/button.svelte';
 </script>
 
@@ -7,14 +7,41 @@
 	<h2 class="h2">Buttons</h2>
 	<Stack class="items-start">
 		<h3 class="h3">Primary</h3>
-		<Button variant="primary" onclick={() => {}}>EOS (0)</Button>
+		<Cluster>
+			<Stack class="gap-2">
+				<p class="caption">Default</p>
+				<Button variant="primary" onclick={() => {}}>EOS (0)</Button>
+			</Stack>
+			<Stack class="gap-2">
+				<p class="caption">Disabled</p>
+				<Button variant="primary" disabled onclick={() => {}}>EOS (0)</Button>
+			</Stack>
+		</Cluster>
 	</Stack>
 	<Stack class="items-start">
 		<h3 class="h3">Secondary</h3>
-		<Button variant="secondary" onclick={() => {}}>EOS (1)</Button>
+		<Cluster>
+			<Stack class="gap-2">
+				<p class="caption">Default</p>
+				<Button variant="secondary" onclick={() => {}}>EOS (1)</Button>
+			</Stack>
+			<Stack class="gap-2">
+				<p class="caption">Disabled</p>
+				<Button variant="secondary" disabled onclick={() => {}}>EOS (1)</Button>
+			</Stack>
+		</Cluster>
 	</Stack>
 	<Stack class="items-start">
 		<h3 class="h3">Pill</h3>
-		<Button variant="pill" onclick={() => {}}>EOS (MAX)</Button>
+		<Cluster>
+			<Stack class="gap-1">
+				<p class="caption">Default</p>
+				<Button variant="pill" onclick={() => {}}>EOS (MAX)</Button>
+			</Stack>
+			<Stack class="gap-1">
+				<p class="caption">Active</p>
+				<Button variant="pill" active={true} onclick={() => {}}>EOS (MAX)</Button>
+			</Stack>
+		</Cluster>
 	</Stack>
 </Stack>
