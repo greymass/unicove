2090f955a48a0aba8283edf54982131651a2612a
Aaron Cox <aaron@greymass.com>
2024-08-14 15:37:25 -0700

    Rewrote redirection to always have a language and network

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import { sourceLanguageTag, availableLanguageTags } from '$lib/paraglide/runtime';
-import { i18n } from '$lib/i18n';
 import type { Handle, RequestEvent } from '@sveltejs/kit';
+
+import { availableLanguageTags } from '$lib/paraglide/runtime';
+import { i18n } from '$lib/i18n';
+import { isNetworkShortName } from '$lib/wharf/chains';
 
 export const i18nHandle = i18n.handle();
 type HandleParams = Parameters<Handle>[0];
@@ -19,24 +21,61 @@ export function getHeaderLang(event: RequestEvent) {
 	return null;
 }
 
+function isAPIPath(pathname: string) {
+	return /^\/[a-z0-9]+\/api/gm.test(pathname);
+}
+
+function isLanguage(value: string) {
+	return availableLanguageTags.find((l) => l.toLowerCase() === value);
+}
+
+function isNetwork(value: string) {
+	return isNetworkShortName(value);
+}
+
 export async function redirectHandle({ event, resolve }: HandleParams): Promise<Response> {
 	const { pathname, search } = new URL(event.request.url);
-	if (/^\/[a-z0-9]+\/api/gm.test(pathname)) {
+
+	if (isAPIPath(pathname)) {
 		return await resolve(event);
 	}
-	const pathLang = pathname.match(/[^/]+?(?=\/|$)/);
-	const matchedLang = pathLang ? pathLang[0].toLowerCase() : null;
-	let lang = availableLanguageTags.find((l) => l.toLowerCase() === matchedLang);
-	if (!lang) {
-		lang = getHeaderLang(event) || sourceLanguageTag;
-		event.locals.lang = lang;
-		const pathnameWithoutLang = pathLang ? pathname.replace(`/${pathLang}`, '') : pathname;
+
+	const [, pathFirst, pathSecond, ...pathMore] = pathname
+		.split('/')
+		.map((p) => p.trim().toLowerCase());
+
+	let lang = 'en';
+	let network = 'eos';
+
+	if (isLanguage(pathFirst) && isNetwork(pathSecond)) {
+		// Proceed, correct URL
+		lang = pathFirst;
+		network = pathSecond;
+	} else if (isLanguage(pathFirst) && !isNetwork(pathSecond)) {
+		// The network isn't specified, but the language is - redirect to the default network
+		lang = pathFirst;
+		pathMore.unshift(pathSecond);
+	} else if (!isLanguage(pathFirst) && isNetwork(pathFirst)) {
+		// The language isn't specified, but the network is - redirect to the default language with the network provided
+		network = pathFirst;
+		pathMore.unshift(pathSecond);
+	} else {
+		// Neither language nor network is specified - redirect to the default language and network
+		pathMore.unshift(pathSecond);
+		pathMore.unshift(pathFirst);
+	}
+
+	event.locals.lang = lang;
+
+	const url = `/${lang}/${network}/${pathMore.join('/')}${search}`;
+
+	if (pathname !== url) {
 		return new Response(undefined, {
-			headers: { Location: `/${lang}${pathnameWithoutLang}${search}` },
+			headers: { Location: url },
 			status: 301
 		});
 	}
-	event.locals.lang = lang;
+
 	const response = await resolve(event);
 	return response;
 }
