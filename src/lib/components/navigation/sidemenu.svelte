e5fda1a8db20455b6415c883182020b70acec192
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-08-19 16:03:39 -0700

    Add active style to side menu

--- src/lib/components/navigation/sidemenu.svelte
+++ src/lib/components/navigation/sidemenu.svelte
@@ -35,14 +35,20 @@
 	class=" sticky top-0 grid h-svh max-h-[768px] min-w-36 grid-rows-[56px_1fr_56px] gap-6 px-9 py-4"
 >
 	<UnicoveLogo wordmark class="" />
-	<nav class="flex flex-col justify-center gap-1 text-nowrap text-base font-medium text-white/50">
+	<nav class="flex flex-col justify-center gap-1 text-nowrap text-base font-medium text-white">
 		{#if context.account}
 			{#each options as option}
-				<a href={option.href} class="flex h-12 items-center">
+				<a
+					href={option.href}
+					class="flex h-12 items-center transition-opacity hover:opacity-100"
+					class:opacity-50={!option.active}
+					class:opacity-100={option.active}
+					aria-current={option.active}
+				>
 					<span
-						class="border-b-2 border-transparent py-1"
+						class="border-b-2 py-1"
+						class:border-transparent={!option.active}
 						class:border-skyBlue-500={option.active}
-						class:text-white={option.active}
 					>
 						{option.text}
 					</span>
