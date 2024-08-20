0f2df6b9115f25598935dc9fd235edab18707407
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-08-20 10:03:02 -0700

    Fix menu options
    Fix menu options

--- src/lib/components/navigation/sidemenu.svelte
+++ src/lib/components/navigation/sidemenu.svelte
@@ -5,56 +5,60 @@
 	import { page } from '$app/stores';
 	import UnicoveLogo from '$lib/assets/unicovelogo.svelte';
 
-	const tabOptions = $derived.by(() => {
-		const account = String(context.account?.name);
+	interface Props {
+		callbackFn?: (event: MouseEvent) => void;
+	}
+
+	let { callbackFn }: Props = $props();
+
+	const destinations = $derived.by(() => {
 		const network = String(context.account?.network);
 		return [
-			{ href: `/${network}/account/${account}`, text: 'Account' },
-			{ href: `/${network}/account/${account}/activity`, text: 'Activity' },
-			{ href: `/${network}/account/${account}/balances`, text: 'Balances' },
-			{ href: `/${network}/account/${account}/permissions`, text: 'Permissions' },
-			{ href: `/${network}/account/${account}/ram`, text: 'RAM' },
-			{ href: `/${network}/account/${account}/resources`, text: 'Resources' },
-			{ href: `/${network}/account/${account}/staked`, text: 'Staked' },
-			{ href: `/${network}/account/${account}/chaindata`, text: 'Chain Data' }
+			{ href: `/${network}/account`, text: 'Account' },
+			{ href: `/${network}/move`, text: 'Move' },
+			{ href: `/${network}/permissions`, text: 'Permissions' },
+			{ href: `/${network}/ram`, text: 'RAM' },
+			{ href: `/${network}/resources`, text: 'Resources' },
+			{ href: `/${network}/send`, text: 'Send' },
+			{ href: `/${network}/transactions`, text: 'Transactions' },
+			{ href: `/${network}/vote`, text: 'Vote' }
 		];
 	});
 
-	let currentTab = $derived($page.url.pathname.split('/').slice(2)[3]);
+	let rootPathname = $derived($page.url.pathname.split('/').slice(2)[1]);
 
 	// Derive the active state of each destination
 	let options = $derived(
-		tabOptions.map((option) => ({
-			...option,
-			active: option.href.split('/').slice(2)[2] === currentTab
+		destinations.map((destination) => ({
+			...destination,
+			active: rootPathname && destination.href.includes(rootPathname)
 		}))
 	);
 </script>
 
 <menu
-	class=" sticky top-0 grid h-svh max-h-[768px] min-w-36 grid-rows-[56px_1fr_56px] gap-6 px-9 py-4"
+	class="sticky top-0 grid h-svh max-h-[768px] min-w-36 grid-rows-[56px_1fr_56px] gap-6 px-9 py-4"
 >
-	<UnicoveLogo wordmark class="" />
+	<UnicoveLogo wordmark class="" {callbackFn} />
 	<nav class="flex flex-col justify-center gap-1 text-nowrap text-base font-medium text-white">
-		{#if context.account}
-			{#each options as option}
-				<a
-					href={option.href}
-					class="flex h-12 items-center transition-opacity hover:opacity-100"
-					class:opacity-50={!option.active}
-					class:opacity-100={option.active}
-					aria-current={option.active}
+		{#each options as option}
+			<a
+				href={option.href}
+				class="flex h-12 items-center transition-opacity hover:opacity-100"
+				class:opacity-50={!option.active}
+				class:opacity-100={option.active}
+				aria-current={!!option.active}
+				onclick={callbackFn}
+			>
+				<span
+					class="border-b-2 py-1"
+					class:border-transparent={!option.active}
+					class:border-skyBlue-500={option.active}
 				>
-					<span
-						class="border-b-2 py-1"
-						class:border-transparent={!option.active}
-						class:border-skyBlue-500={option.active}
-					>
-						{option.text}
-					</span>
-				</a>
-			{/each}
-		{/if}
+					{option.text}
+				</span>
+			</a>
+		{/each}
 	</nav>
 </menu>
 
--- src/routes/[network]/(account)/+layout.svelte
+++ src/routes/[network]/(account)/+layout.svelte
@@ -1,34 +1,47 @@
 <script lang="ts">
-	import AccountNavigation from '$lib/components/navigation/accountnavigation.svelte';
+	import type { UnicoveContext } from '$lib/state/client.svelte';
 	import { page } from '$app/stores';
+	import { getContext } from 'svelte';
+	import { Stack } from '$lib/components/layout';
+	import PillGroup from '$lib/components/navigation/pillgroup.svelte';
 
-	const { children, data } = $props();
+	const { children } = $props();
+	const context = getContext<UnicoveContext>('state');
 
-	const destinations = $derived.by(() => {
-		const network = String(data.network);
+	const tabOptions = $derived.by(() => {
+		const account = String(context.account?.name);
+		const network = String(context.account?.network);
 		return [
-			{ href: `/${network}/account`, text: 'Account' },
-			{ href: `/${network}/move`, text: 'Move' },
-			{ href: `/${network}/permissions`, text: 'Permissions' },
-			{ href: `/${network}/ram`, text: 'RAM' },
-			{ href: `/${network}/resources`, text: 'Resources' },
-			{ href: `/${network}/send`, text: 'Send' },
-			{ href: `/${network}/transactions`, text: 'Transactions' },
-			{ href: `/${network}/vote`, text: 'Vote' }
+			{ href: `/${network}/account/${account}`, text: 'Overview' },
+			{ href: `/${network}/account/${account}/activity`, text: 'Activity' },
+			{ href: `/${network}/account/${account}/balances`, text: 'Balances' },
+			{ href: `/${network}/account/${account}/permissions`, text: 'Permissions' },
+			{ href: `/${network}/account/${account}/ram`, text: 'RAM' },
+			{ href: `/${network}/account/${account}/resources`, text: 'Resources' },
+			{ href: `/${network}/account/${account}/staked`, text: 'Staked' },
+			{ href: `/${network}/account/${account}/chaindata`, text: 'Chain Data' }
 		];
 	});
 
-	let rootPathname = $derived($page.url.pathname.split('/').slice(2)[1]);
+	let currentTab = $derived($page.url.pathname.split('/').slice(2)[3]);
 
 	// Derive the active state of each destination
 	let options = $derived(
-		destinations.map((destination) => ({
-			...destination,
-			active: destination.href.includes(rootPathname)
+		tabOptions.map((option) => ({
+			...option,
+			active: option.href.split('/').slice(2)[2] === currentTab
 		}))
 	);
 </script>
 
-<AccountNavigation {options} />
+<header class="layout-stack gap-6">
+	<Stack class="gap-2">
+		<h1 class="h2 leading-none text-white/60">Account</h1>
+		{#if context.account}
+			<h2 class="h1 font-bold text-white">{context.account.name}</h2>
+		{/if}
+	</Stack>
+	<PillGroup {options} class="mb-6" />
+</header>
 
 {@render children()}
