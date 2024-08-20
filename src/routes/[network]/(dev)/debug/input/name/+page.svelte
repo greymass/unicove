08d47ca4fc5a0f55d631974d04d5a792e1a573b3
Aaron Cox <aaron@greymass.com>
2024-08-20 16:02:24 -0700

    Name input

---
+++ src/routes/[network]/(dev)/debug/input/name/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { Name } from '@wharfkit/antelope';
+
+	import NameInput from '$lib/components/input/name.svelte';
+	import Button from '$lib/components/button/button.svelte';
+	import { Stack } from '$lib/components/layout';
+
+	let input: NameInput;
+
+	let value = $state(Name.from('greymass'));
+	let valid = $state(false);
+</script>
+
+<Stack class="items-start">
+	<span>
+		<Button variant="secondary" onclick={() => input.set('greymass')}>Valid</Button>
+		<Button variant="secondary" onclick={() => input.set('greymass.gm')}>Valid (Dot)</Button>
+		<Button variant="secondary" onclick={() => input.set('Greymass')}>Invalid (Casing)</Button>
+		<Button variant="secondary" onclick={() => input.set('greymassgreymass')}
+			>Invalid (Length)</Button
+		>
+		<Button variant="secondary" onclick={() => input.set('123456')}>Invalid (Number >5)</Button>
+		<Button variant="secondary" onclick={() => input.set('big$')}>Invalid (Symbol)</Button>
+	</span>
+
+	<Button disabled={!valid} onclick={() => alert(value)}>Do a thing with the name!</Button>
+
+	<div>
+		<label>
+			Enter a valid name:
+			<NameInput bind:this={input} autofocus bind:value bind:valid debug />
+		</label>
+	</div>
+
+	<div>
+		<h3>Page State</h3>
+		<pre>
+			Valid Input: {valid}
+			Name: {value}
+		</pre>
+	</div>
+</Stack>
