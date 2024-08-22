dfe60a06c03e33151688022b8090e4349da2460d
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-08-22 08:05:50 -0700

    Add some padding to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script>
 	import * as m from '$lib/paraglide/messages.js';
 	import Button from '$lib/components/button/button.svelte';
-	import { Stack } from '$lib/components/layout';
+	import { Box, Stack } from '$lib/components/layout';
 	import { languageTag } from '$lib/paraglide/runtime';
 	import { chainShortNames } from '$lib/wharf/chains';
 </script>
 
-<Stack>
-	<p>Language: {languageTag()}</p>
-	<p>Test Localization String: {m.test_locale_string()}</p>
+<Box>
+	<Stack>
+		<p>Language: {languageTag()}</p>
+		<p>Test Localization String: {m.test_locale_string()}</p>
 
-	<p>Select a network</p>
-	{#each chainShortNames as chain}
-		<Button href={`/${chain}`}>{chain}</Button>
-	{/each}
-</Stack>
+		<p>Select a network</p>
+		{#each chainShortNames as chain}
+			<Button href={`/${chain}`}>{chain}</Button>
+		{/each}
+	</Stack>
+</Box>
