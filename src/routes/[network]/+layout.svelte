97a94d53bbb40a4be05087c87b8790ac7317b370
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-09-11 15:29:40 -0700

    style: fix mobile layout

--- src/routes/[network]/+layout.svelte
+++ src/routes/[network]/+layout.svelte
@@ -70,25 +70,31 @@
 	});
 </script>
 
+<!-- This is the main layout using grid columns and rows to align elements that appear at specific breakpoints -->
 <div
 	class="
 	relative
 	grid
 	h-full
+	min-h-svh
 	w-svw
 	max-w-screen-xl
 	grid-cols-[16px_auto_auto_16px]
-	grid-rows-[16px_auto_auto_16px]
+	grid-rows-[0_min-content_auto_56px]
+	gap-y-4
 	bg-shark-950
 	md:h-auto
 	md:min-h-svh
 	md:grid-cols-[auto_minmax(0,1fr)_auto_auto_32px]
-	md:grid-rows-[16px_minmax(0,1fr)]
+	md:grid-rows-[16px_minmax(0,1fr)_16px]
 	md:gap-x-4
+	md:gap-y-0
 	"
 >
+	<!-- This is the small unicove logo that appears on mobile only -->
 	<UnicoveLogo small network={data.network} class="col-start-2 row-start-2 w-min md:hidden" />
 
+	<!-- The account switcher remains in the top right on mobile and desktop -->
 	<div
 		class="col-start-3 row-start-2 flex justify-end md:col-start-4 md:col-end-5 md:row-start-2 md:row-end-3 md:px-4"
 	>
@@ -96,19 +102,22 @@
 	</div>
 
 	<main
-		class="col-span-2 col-start-2 row-start-3 overflow-hidden md:col-span-3 md:col-start-2 md:row-start-2 md:row-end-4"
+		class="col-span-2 col-start-2 row-start-3 md:col-span-3 md:col-start-2 md:row-start-2 md:row-end-4"
 	>
 		{@render children()}
 	</main>
 
+	<!-- This wrapper sets up the bottom menu on mobile, becomes a pass-thru component on desktop -->
 	<aside
 		class="fixed bottom-0 z-50 col-span-4 grid h-14 w-full grid-cols-subgrid bg-shark-950 px-4 py-2 md:contents"
 	>
+		<!-- This element is the sidebar on desktop, collapsible menu on mobile -->
 		<MobileNavigation
 			network={data.network}
-			class="col-start-1 col-end-2 md:row-start-1 md:row-end-3"
+			class="col-start-1 col-end-2 md:row-start-1 md:row-end-4"
 		/>
 
+		<!-- This search box is in the bottom menu on mobile, top header on desktop -->
 		<Search
 			network={data.network}
 			class="col-start-2 col-end-3  md:col-start-3 md:col-end-4 md:row-start-2 md:row-end-3"
