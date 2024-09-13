ce2fd36ab32907d7cb44f338a440affaebf70b74
dafuga <danielfugere28@gmail.com>
2024-09-12 20:50:17 -0700

    chore: adding account ram header to the ram overview page

--- src/routes/[network]/(account)/ram/+page.svelte
+++ src/routes/[network]/(account)/ram/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import RamPriceHistory from '$lib/components/chart/rampricehistory.svelte';
+	import RAM from '$lib/components/elements/ram.svelte';
+	import { getContext } from 'svelte';
+	import type { UnicoveContext } from '$lib/state/client.svelte';
+
 	const { data } = $props();
+	const context = getContext<UnicoveContext>('state');
 </script>
+
+<h3>Your RAM</h3>
+{#if context.account && context.account.ram}
+	<p>Total: <RAM bytes={Number(context.account.ram.max || 0)} /></p>
+	<p>Available: <RAM bytes={Number(context.account.ram.available || 0)} /></p>
+	<p>Used: <RAM bytes={Number(context.account.ram.used || 0)} /></p>
+{:else}
+	<p>Loading your RAM information...</p>
+{/if}
 
 <h3>Current RAM Price</h3>
 {#if data.network && data.network.ramstate}
