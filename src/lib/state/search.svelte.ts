ec60e6e158219e66b4169cea3430eca705860aa7
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-10-04 05:32:22 -0700

    feat(search): add max length to history

--- src/lib/state/search.svelte.ts
+++ src/lib/state/search.svelte.ts
@@ -3,8 +3,13 @@ type SearchResult = {
 	searchType: string;
 };
 
+const maxHistoryLength = 10;
+
 export const history = $state<SearchResult[]>([]);
 
 export function addHistory(s: SearchResult) {
 	history.unshift(s);
+	if (history.length > maxHistoryLength) {
+		history.splice(maxHistoryLength);
+	}
 }
