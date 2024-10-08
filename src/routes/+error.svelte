e613257003c53269082ee9e32256d7da5de9a502
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-10-08 12:42:03 -0700

    feat: basic error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,6 @@
 	import { page } from '$app/stores';
 </script>
 
+<!-- This error page would get triggered if the user entered an invalid URL like /en/eos/asdf  -->
+<!-- TODO: We'll need some alternative styling here since it doesn't inherit any layout from the rest of the site. -->
 <pre>{JSON.stringify($page.error, null, 2)}</pre>
