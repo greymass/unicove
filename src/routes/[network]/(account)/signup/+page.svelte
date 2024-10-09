8b4f5b2c71d7d0c5005a9cfd66d5ab2116697c3a
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-10-09 14:24:20 -0700

    style: wallet selection

--- src/routes/[network]/(account)/signup/+page.svelte
+++ src/routes/[network]/(account)/signup/+page.svelte
@@ -25,31 +25,27 @@ hover:bg-mineShaft-950 focus-visible:outline focus-visible:outline-transparent f
 	</a>
 {/snippet}
 
-<Stack
-	class="gap-7 *:z-10 before:absolute before:-inset-4 before:z-0 before:bg-shark-900/10 md:mx-auto md:max-w-md md:p-6 md:before:inset-0 md:before:rounded-2xl"
->
-	<Stack class="gap-2">
-		<h3 class="h3">Why do I need a wallet?</h3>
-		<p>
-			A wallet is your gateway to the blockchain, allowing you to manage your digital assets and
-			interact with decentralized applications.
-		</p>
-	</Stack>
+<Stack class="gap-2">
+	<h3 class="h3">Why do I need a wallet?</h3>
+	<p>
+		A wallet is your gateway to the blockchain, allowing you to manage your digital assets and
+		interact with decentralized applications.
+	</p>
+</Stack>
 
-	<Stack class="gap-2">
-		<h3 class="h3">Why do I need an account?</h3>
-		<p>
-			An account on the blockchain is your unique identity, enabling you to perform transactions,
-			store assets, and participate in the network.
-		</p>
-	</Stack>
+<Stack class="gap-2">
+	<h3 class="h3">Why do I need an account?</h3>
+	<p>
+		An account on the blockchain is your unique identity, enabling you to perform transactions,
+		store assets, and participate in the network.
+	</p>
+</Stack>
 
+<Stack>
+	<h3 class="h2">Choose A Wallet Type</h3>
 	<Stack>
-		<h3 class="h2">Choose A Wallet Type</h3>
-		<Stack>
-			{#each Object.values(walletTypes) as wallet}
-				{@render walletButton(wallet)}
-			{/each}
-		</Stack>
+		{#each Object.values(walletTypes) as wallet}
+			{@render walletButton(wallet)}
+		{/each}
 	</Stack>
 </Stack>
