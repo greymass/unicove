9cf423d12b51b7432be6e5b6bd0d9264127788cd
kurt <ttwishing@gmail.com>
2024-11-08 10:36:42 +0800

    add bottom padding for renting page on mobile screen

--- src/routes/[network]/(account)/resources/powerup/+page.svelte
+++ src/routes/[network]/(account)/resources/powerup/+page.svelte
@@ -12,6 +12,6 @@
 	const account = $derived(context.account);
 </script>
 
-<div class="container rounded-2xl sm:bg-shark-900/20 sm:p-6 sm:pb-10">
+<div class="container rounded-2xl pb-4 sm:bg-shark-900/20 sm:p-6 sm:pb-10">
 	<Renting rentType="POWERUP" {network} {account}></Renting>
 </div>
