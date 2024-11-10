5a76f9cd93b432087f44d8585f6711118f427ac7
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-09 17:05:44 -0800

    style: simplify css

--- src/lib/components/elements/cpunetresource.svelte
+++ src/lib/components/elements/cpunetresource.svelte
@@ -10,25 +10,28 @@
 	const { cpuAvailable, netAvailable, precision = 2 }: Props = $props();
 </script>
 
-<div class="flex gap-px">
-	<div class="relative h-28 flex-1 rounded-l-xl bg-[#303338]">
-		<div class="absolute left-4 top-5">
+<div class="flex gap-px text-right *:flex-1 *:bg-mineShaft-900/70 *:p-4 *:pb-2">
+	<div class="rounded-l-lg">
+		<h3 class="mb-4 flex items-center justify-between text-xl font-bold">
 			<CpuIcon class="size-6" />
-		</div>
-		<div class="absolute right-3 top-3 text-xl font-bold">CPU</div>
-		<div class="absolute bottom-2 right-3">
-			<h5 class="h5 text-right">{cpuAvailable.toFixed(precision)} ms</h5>
-			<p>Available</p>
-		</div>
+			CPU
+		</h3>
+
+		<p class="*:block">
+			<span class="text-white"> {cpuAvailable.toFixed(precision)} ms </span>
+			<span>Available</span>
+		</p>
 	</div>
-	<div class="relative h-28 flex-1 rounded-r-xl bg-[#303338]">
-		<div class="absolute left-5 top-4">
+
+	<div class="rounded-r-lg">
+		<h3 class="mb-4 flex items-center justify-between text-xl font-bold">
 			<WifiIcon class="size-6" />
-		</div>
-		<div class="absolute right-4 top-3 text-xl font-bold">NET</div>
-		<div class="absolute bottom-2 right-4">
-			<h5 class="h5 text-right">{netAvailable.toFixed(precision)} kb</h5>
-			<p>Available</p>
-		</div>
+			NET
+		</h3>
+
+		<p class="*:block">
+			<span class="text-white"> {netAvailable.toFixed(precision)} kb </span>
+			<span>Available</span>
+		</p>
 	</div>
 </div>
