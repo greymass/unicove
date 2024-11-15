14c18fd913efb6425f6f4cfdcca25a7352b99040
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-14 17:14:59 -0800

    enhancement: asset text currency formatting

--- src/lib/components/elements/asset.svelte
+++ src/lib/components/elements/asset.svelte
@@ -3,32 +3,56 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	import { languageTag } from '$lib/paraglide/runtime.js';
+	import { cn } from '$lib/utils';
+	import { getSetting } from '$lib/state/settings.svelte';
+
+	const advancedMode = getSetting('advanced-mode', false);
 
 	interface AssetProps extends HTMLAttributes<HTMLSpanElement> {
 		value?: Asset;
 		variant?: 'value' | 'full';
-		fallback?: string;
+		fallback?: number;
 	}
 
-	let {
-		class: className = '',
-		value,
-		variant = 'value',
-		fallback = '0',
-		...props
-	}: AssetProps = $props();
+	let { value: asset, variant = 'value', fallback = 0, ...props }: AssetProps = $props();
 
-	function formatAssetValue(value?: Asset) {
-		return Number(value?.value || fallback).toLocaleString(languageTag(), {
-			minimumFractionDigits: value?.symbol.precision
-		});
+	// TODO: Move somewhere else.
+	const SUPPORTED_CURRENCIES = ['USD'];
+
+	const locale = languageTag();
+
+	const assetOptions: Intl.NumberFormatOptions = {
+		minimumFractionDigits: asset?.symbol.precision
+	};
+
+	const currencyOptions: Intl.NumberFormatOptions = {
+		style: 'currency',
+		currency: asset?.symbol.name,
+		minimumFractionDigits: advancedMode.value ? asset?.symbol.precision : undefined
+	};
+
+	function formatAssetValue() {
+		return Intl.NumberFormat(locale, assetOptions).format(asset?.value || fallback);
 	}
+
+	function formatCurrencyValue() {
+		console.log(currencyOptions);
+		return Intl.NumberFormat(locale, currencyOptions).format(asset?.value || fallback);
+	}
+
+	const isCurrency = SUPPORTED_CURRENCIES.includes(String(asset?.symbol.name));
 </script>
 
-<span class={className} {...props}>
-	{#if variant === 'full'}
-		{value ? `${formatAssetValue(value)} ${value.symbol.name}` : fallback}
-	{:else}
-		{formatAssetValue(value)}
-	{/if}
-</span>
+{#snippet span(string: string)}
+	<span class={cn('text-right tabular-nums', props.class)} {...props}>{string}</span>
+{/snippet}
+
+{#if isCurrency && variant === 'value'}
+	{@render span(formatCurrencyValue())}
+{:else if isCurrency && variant === 'full'}
+	{@render span(`${formatCurrencyValue()} ${asset?.symbol.name}`)}
+{:else if !isCurrency && variant === 'value'}
+	{@render span(formatAssetValue())}
+{:else if !isCurrency && variant === 'full'}
+	{@render span(`${formatAssetValue()} ${asset?.symbol.name}`)}
+{/if}
