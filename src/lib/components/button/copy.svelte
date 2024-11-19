ead497042f2e00136b57e637fe9b051d489722e4
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-19 13:57:15 -0800

    style: tweak animation

--- src/lib/components/button/copy.svelte
+++ src/lib/components/button/copy.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { getSetting } from '$lib/state/settings.svelte';
 	import Copy from 'lucide-svelte/icons/copy';
-	import { quartOut, sineIn } from 'svelte/easing';
+	import { quadIn, quartOut } from 'svelte/easing';
 	import { fade, fly } from 'svelte/transition';
 
 	interface Props {
@@ -19,7 +19,7 @@
 			await navigator.clipboard.writeText(props.data);
 			if (debugMode) console.log(props.data, 'copied to clipboard');
 			hint = true;
-			setTimeout(() => (hint = false), 500);
+			setTimeout(() => (hint = false), 300);
 		} catch (err) {
 			if (debugMode) console.error('Failed to copy text: ', err);
 		}
@@ -31,14 +31,14 @@
 {#if 'clipboard' in navigator}
 	<button
 		onclick={copyToClipboard}
-		class="absolute left-full grid size-12 -translate-x-2 translate-y-1 place-items-center gap-2 text-skyBlue-500 hover:text-skyBlue-400 focus-visible:text-skyBlue-400 focus-visible:outline-none active:text-skyBlue-200"
+		class="absolute left-full grid size-12 -translate-x-2 translate-y-1 place-items-center gap-2 text-skyBlue-500 hover:text-skyBlue-400 focus-visible:text-skyBlue-400 focus-visible:outline-none active:scale-95"
 	>
 		<div class="relative">
 			<Copy class="size-4 " />
 			{#if hint}
 				<span
-					in:fly={{ x: -20, easing: quartOut }}
-					out:fade={{ easing: sineIn }}
+					in:fly={{ x: -20, easing: quartOut, duration: 100 }}
+					out:fade={{ easing: quadIn, duration: 200 }}
 					class="absolute inset-y-0 left-full translate-x-2 text-xs text-skyBlue-400">Copied!</span
 				>
 			{/if}
