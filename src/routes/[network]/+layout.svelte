6d420df6ee04cda80bc769c24eb3c2d33568c32a
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-20 14:31:13 -0800

    dev: update interval in env

--- src/routes/[network]/+layout.svelte
+++ src/routes/[network]/+layout.svelte
@@ -12,6 +12,7 @@
 	import { AccountState } from '$lib/state/client/account.svelte.js';
 	import { WharfState } from '$lib/state/client/wharf.svelte.js';
 	import { NetworkState, getNetwork } from '$lib/state/network.svelte.js';
+	import * as env from '$env/static/public';
 
 	let { children, data } = $props();
 
@@ -85,8 +86,8 @@
 	});
 
 	// Number of ms between network updates
-	const ACCOUNT_UPDATE_INTERVAL = 3_000;
-	const NETWORK_UPDATE_INTERVAL = 3_000;
+	const ACCOUNT_UPDATE_INTERVAL = Number(env.PUBLIC_ACCOUNT_UPDATE_INTERVAL) || 3_000;
+	const NETWORK_UPDATE_INTERVAL = Number(env.PUBLIC_NETWORK_UPDATE_INTERVAL) || 3_000;
 
 	// Default to not show a banner (avoids flash of banner when hidden)
 	let showBanner = $state(false);
