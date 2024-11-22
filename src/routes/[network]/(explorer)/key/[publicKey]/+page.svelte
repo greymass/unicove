1b3cb78377d805b4fb80e378877a39823e7a20e2
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-22 15:25:38 -0800

    style: key page changes

--- src/routes/[network]/(explorer)/key/[publicKey]/+page.svelte
+++ src/routes/[network]/(explorer)/key/[publicKey]/+page.svelte
@@ -1,43 +1,46 @@
 <script lang="ts">
-	import Card from '$lib/components/layout/box/card.svelte';
+	import { Card, Cluster, MultiCard } from '$lib/components/layout';
 	import * as m from '$lib/paraglide/messages.js';
-	import { ArrowRight } from 'lucide-svelte';
+	import Account from '$lib/components/elements/account.svelte';
+	import Key from 'lucide-svelte/icons/key-round';
 
 	let { data } = $props();
 </script>
 
-<Card class="max-w-lg">
-	<h4 class="mb-4 text-center">{m.accounts_using_public_key()}:</h4>
-
-	{#await data.accounts}
-		<!-- TODO: Generic loading component -->
-		<p class="text-muted text-center italic">{m.loading_accounts()}</p>
-	{:then accounts}
-		{#if accounts && accounts.length > 0}
+<MultiCard>
+	<Card>
+		<Cluster class="items-center">
+			<picture class="grid size-12 place-items-center rounded-full bg-mineShaft-900">
+				<Key />
+			</picture>
 			<div>
-				{#each accounts as account}
-					<div
-						class="flex flex-wrap items-center justify-between gap-x-4 border-b border-mineShaft-900 py-3 *:grow last:border-none"
-					>
-						<dt class="caption">{account}</dt>
-						<dd class="text-right tabular-nums">
-							<a
-								href="/{data.network}/account/{account}"
-								class="text-skyBlue-500 hover:text-skyBlue-400"
-							>
-								<span class="inline-flex items-center gap-2">
-									{m.account_page()}
-									<ArrowRight size={20} />
-								</span>
-							</a>
-						</dd>
-					</div>
-				{/each}
+				<!-- TODO: Display publicKey in multiple formats -->
+				<p>{data.publicKey}</p>
+				<p>{data.publicKey}</p>
 			</div>
-		{:else}
-			<p class="text-muted text-center">{m.no_accounts_found()}</p>
-		{/if}
-	{:catch error}
-		<p class="text-center text-red-600">{m.error_loading_accounts({ error: error.message })}</p>
-	{/await}
-</Card>
+		</Cluster>
+	</Card>
+	<Card title={m.accounts_using_public_key()}>
+		{#await data.accounts}
+			<!-- TODO: Create generic loading component -->
+			<p class="text-muted text-center italic">{m.loading_accounts()}</p>
+		{:then accounts}
+			{#if accounts && accounts.length > 0}
+				<Cluster tag="ul">
+					{#each accounts as account}
+						<li class="flex">
+							<Account
+								name={account}
+								class="h-12 content-center rounded-lg px-4 hover:bg-mineShaft-900 hover:text-mineShaft-50 focus-visible:bg-mineShaft-900 focus-visible:text-mineShaft-50"
+							/>
+						</li>
+					{/each}
+				</Cluster>
+			{:else}
+				<p class="text-muted text-center">{m.no_accounts_found()}</p>
+			{/if}
+		{:catch error}
+			<p class="text-center text-red-600">{m.error_loading_accounts({ error: error.message })}</p>
+		{/await}</Card
+	>
+</MultiCard>
