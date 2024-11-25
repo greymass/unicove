89119757eb0827361aa8d064022e6dafaf40dc95
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-25 14:51:01 -0800

    fix: use correct Load type

--- src/routes/[network]/(explorer)/key/[publicKey]/+page.ts
+++ src/routes/[network]/(explorer)/key/[publicKey]/+page.ts
@@ -1,8 +1,8 @@
-import type { Load } from '@sveltejs/kit';
 import * as m from '$lib/paraglide/messages.js';
 import { PublicKey } from '@wharfkit/antelope';
+import type { PageLoad } from './$types';
 
-export const load: Load = async ({ fetch, params, parent }) => {
+export const load: PageLoad = async ({ fetch, params, parent }) => {
 	const { network } = await parent();
 	const accountsPromise = fetch(`/${params.network}/api/key/${params.publicKey}`)
 		.then((response) => response.json())
