07120b2ac6bf0eb80cd7151bdeac4bf231a1ce0b
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-25 10:56:31 -0800

    feat: hide account data tab under debug flag

--- src/routes/[network]/(explorer)/account/[name]/+layout.svelte
+++ src/routes/[network]/(explorer)/account/[name]/+layout.svelte
@@ -2,22 +2,30 @@
 	import { page } from '$app/stores';
 	import { Stack } from '$lib/components/layout';
 	import PillGroup from '$lib/components/navigation/pillgroup.svelte';
+	import { getSetting } from '$lib/state/settings.svelte.js';
 
 	const { children, data } = $props();
+
+	const { value: debugMode } = $derived(getSetting('debug-mode', false));
 
 	const tabOptions = $derived.by(() => {
 		const account = String(data.account.name);
 		const network = String(data.account.network);
-		return [
+		let items = [
 			{ href: `/${network}/account/${account}`, text: 'Overview' },
 			{ href: `/${network}/account/${account}/activity`, text: 'Activity' },
-			{ href: `/${network}/account/${account}/balances`, text: 'Balances' },
+			{ href: `/${network}/account/${account}/balances`, text: 'Balances' }
 			// { href: `/${network}/account/${account}/permissions`, text: 'Permissions' },
 			// { href: `/${network}/account/${account}/ram`, text: 'RAM' },
 			// { href: `/${network}/account/${account}/resources`, text: 'Resources' },
 			// { href: `/${network}/account/${account}/staked`, text: 'Staked' },
-			{ href: `/${network}/account/${account}/chaindata`, text: 'Data' }
 		];
+
+		if (debugMode) {
+			items.push({ href: `/${network}/account/${account}/chaindata`, text: 'Data' });
+		}
+
+		return items;
 	});
 
 	let currentTab = $derived($page.url.pathname.split('/').slice(2)[3]);
