6fc34363ac50170f1a2b59d3b564dfa20dad65c9
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-25 15:25:20 -0800

    feat: add description to settings label

--- src/routes/[network]/settings/+page.svelte
+++ src/routes/[network]/settings/+page.svelte
@@ -3,6 +3,7 @@
 	import LanguageSelect from '$lib/components/select/language.svelte';
 	import { getSetting } from '$lib/state/settings.svelte';
 	import Label from '$lib/components/input/label.svelte';
+	import { Stack } from '$lib/components/layout';
 
 	let advancedMode = getSetting('advanced-mode', false);
 	let debugMode = getSetting('debug-mode', false);
@@ -15,25 +16,41 @@
 	});
 </script>
 
-<div class="grid max-w-screen-sm auto-rows-fr gap-4">
+<div class="grid max-w-screen-sm auto-rows-fr gap-6">
 	<div class="flex items-center justify-between">
-		<Label for="language-select">Language Selector</Label>
+		<Stack class="gap-1">
+			<Label for="language-select">Language Selector</Label>
+			<!-- <p class="caption text-sm">Choose a language</p> -->
+		</Stack>
 		<LanguageSelect />
 	</div>
 
 	<div class="flex items-center justify-between">
-		<Label for="advanced-mode">Enable Advanced Mode</Label>
+		<Stack class="gap-1">
+			<Label for="advanced-mode">Enable Advanced Mode</Label>
+			<p class="caption text-sm">
+				View Resources, Enhanced asset precision, extra options and more
+			</p>
+		</Stack>
 		<Switch id="advanced-mode" bind:checked={advancedMode.value} />
 	</div>
 
 	{#if advancedMode.value}
 		<div class="flex items-center justify-between">
-			<Label for="debug-mode">Enable Debug Mode</Label>
+			<Stack class="gap-1">
+				<Label for="debug-mode">Enable Debug Mode</Label>
+				<p class="caption text-sm">Show raw data used for development and debugging</p>
+			</Stack>
 			<Switch id="debug-mode" bind:checked={debugMode.value} />
 		</div>
 
 		<div class="flex items-center justify-between">
-			<Label for="advanced-mode">Stay on current page when switching accounts</Label>
+			<Stack class="gap-1">
+				<Label for="advanced-mode">Prevent navigation on account switching</Label>
+				<p class="caption text-sm">
+					Stay on the current page instead of navigating to account overview
+				</p>
+			</Stack>
 			<Switch id="advanced-mode" bind:checked={preventAccountPageSwitching.value} />
 		</div>
 	{/if}
