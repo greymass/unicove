6b11751fe0499dd9a8ae3e3568e4df4a5daa4ec0
Aaron Cox <aaron@greymass.com>
2024-11-28 11:23:00 -0800

    Adding routePath for debugging

--- src/lib/components/pageheader.svelte
+++ src/lib/components/pageheader.svelte
@@ -49,6 +49,7 @@
 	<div class="grid gap-2">
 		<h1 class="w-fit text-3xl font-bold leading-none text-white">
 			<span>{props.title}</span>
+			{routePath}
 			{#if routePath === 'account'}
 				<CopyButton data={props.title} />
 			{/if}
