9f8a66a1d04990f7075428a9bf6c47e1da569c01
Aaron Cox <aaron@greymass.com>
2024-11-28 14:18:56 -0800

    Localized settings page

--- src/routes/[network]/settings/+page.svelte
+++ src/routes/[network]/settings/+page.svelte
@@ -6,6 +6,7 @@
 	import type { UnicoveContext } from '$lib/state/client.svelte';
 	import { getContext } from 'svelte';
 	import Code from '$lib/components/code.svelte';
+	import * as m from '$lib/paraglide/messages';
 
 	const context = getContext<UnicoveContext>('state');
 
@@ -29,12 +30,12 @@
 
 <div class="grid max-w-screen-sm auto-rows-fr gap-6">
 	<div class="flex items-center justify-between">
-		<h2 class="text-muted text-xl font-semibold">General</h2>
+		<h2 class="text-muted text-xl font-semibold">{m.settings_general()}</h2>
 	</div>
 
 	<div class="flex items-center justify-between">
 		<Stack class="gap-1">
-			<Label for="language-select">Language Selector</Label>
+			<Label for="language-select">{m.settings_language_selector()}</Label>
 			<!-- <p class="caption text-sm">Choose a language</p> -->
 		</Stack>
 		<LanguageSelect />
@@ -42,16 +43,16 @@
 
 	<div class="flex items-center justify-between">
 		<Stack class="gap-1">
-			<Label for="search-show-pages">Allow page navigation in search prompt</Label>
-			<p class="caption text-sm">Adds popular Unicove pages to search results.</p>
+			<Label for="search-show-pages">{m.settings_search_show_pages()}</Label>
+			<p class="caption text-sm">{m.settings_search_show_pages_desc()}</p>
 		</Stack>
 		<Switch id="search-show-pages" bind:checked={searchShowPages} />
 	</div>
 
 	<div class="flex items-center justify-between">
 		<Stack class="gap-1">
-			<Label for="search-account-switch">Allow account switching in search prompt</Label>
-			<p class="caption text-sm">Adds logged in accounts to search results for quick switching.</p>
+			<Label for="search-account-switch">{m.settings_search_account_switch()}</Label>
+			<p class="caption text-sm">{m.settings_search_account_switch_desc()}</p>
 		</Stack>
 		<Switch id="search-account-switch" bind:checked={searchAccountSwitch} />
 	</div>
@@ -59,33 +60,33 @@
 	<div class="flex items-center justify-between">
 		<Stack class="gap-1">
 			<Label for="prevent-account-page-switching"
-				>Remain on current page when switching accounts</Label
+				>{m.settings_prevent_account_page_switching()}</Label
 			>
-			<p class="caption text-sm">Prevents the automatic navigation to the account overview page</p>
+			<p class="caption text-sm">{m.settings_prevent_account_page_switching_desc()}</p>
 		</Stack>
 		<Switch id="prevent-account-page-switching" bind:checked={preventAccountPageSwitching} />
 	</div>
 
 	<div class="flex items-center justify-between">
-		<h2 class="text-muted text-xl font-semibold">Advanced</h2>
+		<h2 class="text-muted text-xl font-semibold">{m.settings_advanced()}</h2>
 	</div>
 
 	<div class="flex items-center justify-between">
 		<Stack class="gap-1">
-			<Label for="advanced-mode">Enable Advanced Options</Label>
-			<p class="caption text-sm">Manage network resources, enhanced asset precision, etc.</p>
+			<Label for="advanced-mode">{m.settings_enable_advanced()}</Label>
+			<p class="caption text-sm">{m.settings_enable_advanced_desc()}</p>
 		</Stack>
 		<Switch id="advanced-mode" bind:checked={advancedMode} />
 	</div>
 
 	{#if advancedMode}
 		<div class="flex items-center justify-between">
-			<h2 class="text-muted text-xl font-semibold">Developer</h2>
+			<h2 class="text-muted text-xl font-semibold">{m.settings_developer()}</h2>
 		</div>
 		<div class="flex items-center justify-between">
 			<Stack class="gap-1">
-				<Label for="debug-mode">Enable Debug Mode</Label>
-				<p class="caption text-sm">Show raw data used for development and debugging</p>
+				<Label for="debug-mode">{m.settings_enable_developer()}</Label>
+				<p class="caption text-sm">{m.settings_enable_developer_desc()}</p>
 			</Stack>
 			<Switch id="debug-mode" bind:checked={debugMode} />
 		</div>
