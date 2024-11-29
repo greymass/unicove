a48a375aec645a3072caf14a5ccefc782fca035d
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-11-29 15:00:38 -0800

    fix: empty state for tx page

--- src/routes/[network]/(explorer)/transaction/[id]/[[seq]]/+page.svelte
+++ src/routes/[network]/(explorer)/transaction/[id]/[[seq]]/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
 	import Code from '$lib/components/code.svelte';
 
-	import SendSummary from '$lib/components/summary/eosio.token/transfer.svelte';
-	import DepositSummary from '$lib/components/summary/eosio/deposit.svelte';
-	import WithdrawSummary from '$lib/components/summary/eosio/withdraw.svelte';
-	import BuyREXSummary from '$lib/components/summary/eosio/buyrex.svelte';
-	import SellREXSummary from '$lib/components/summary/eosio/sellrex.svelte';
-	import MvfrsavingsSummary from '$lib/components/summary/eosio/mvfrsavings.svelte';
+	// import SendSummary from '$lib/components/summary/eosio.token/transfer.svelte';
+	// import DepositSummary from '$lib/components/summary/eosio/deposit.svelte';
+	// import WithdrawSummary from '$lib/components/summary/eosio/withdraw.svelte';
+	// import BuyREXSummary from '$lib/components/summary/eosio/buyrex.svelte';
+	// import SellREXSummary from '$lib/components/summary/eosio/sellrex.svelte';
+	// import MvfrsavingsSummary from '$lib/components/summary/eosio/mvfrsavings.svelte';
 
-	const summaryMap = {
-		'eosio.token': {
-			transfer: SendSummary
-		},
-		eosio: {
-			deposit: DepositSummary,
-			withdraw: WithdrawSummary,
-			buyrex: BuyREXSummary,
-			sellrex: SellREXSummary,
-			mvfrsavings: MvfrsavingsSummary
-		}
-	} as const;
+	// const summaryMap = {
+	// 	'eosio.token': {
+	// 		transfer: SendSummary
+	// 	},
+	// 	eosio: {
+	// 		deposit: DepositSummary,
+	// 		withdraw: WithdrawSummary,
+	// 		buyrex: BuyREXSummary,
+	// 		sellrex: SellREXSummary,
+	// 		mvfrsavings: MvfrsavingsSummary
+	// 	}
+	// } as const;
 
 	let { data } = $props();
 
@@ -32,31 +32,31 @@
 	}
 
 	// Type guard for account
-	function isValidAccount(account: string): account is keyof typeof summaryMap {
-		return account in summaryMap;
-	}
+	// function isValidAccount(account: string): account is keyof typeof summaryMap {
+	// 	return account in summaryMap;
+	// }
 
 	// Type guard for action name
-	function isValidActionName(
-		account: keyof typeof summaryMap,
-		name: string
-	): name is keyof (typeof summaryMap)[typeof account] {
-		return name in summaryMap[account];
-	}
+	// function isValidActionName(
+	// 	account: keyof typeof summaryMap,
+	// 	name: string
+	// ): name is keyof (typeof summaryMap)[typeof account] {
+	// 	return name in summaryMap[account];
+	// }
 </script>
 
-<table class="table-styles">
-	<thead>
-		<tr>
-			<th>Contract</th>
-			<th>Action</th>
-			<th>Authorization</th>
-			<th>Data</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#if data.transaction && data.transaction.trx}
-			{@const actions = data.transaction.trx.trx.actions as Action[]}
+{#if data.transaction?.trx?.actions}
+	{@const actions = data.transaction.trx.trx.actions as Action[]}
+	<table class="table-styles">
+		<thead>
+			<tr>
+				<th>Contract</th>
+				<th>Action</th>
+				<th>Authorization</th>
+				<th>Data</th>
+			</tr>
+		</thead>
+		<tbody>
 			{#each actions as action}
 				<tr>
 					<td>
@@ -83,36 +83,37 @@
 					</td>
 				</tr>
 			{/each}
-		{/if}
-	</tbody>
-</table>
+		</tbody>
+	</table>
+{:else}
+	<p>No actions</p>
+{/if}
 
-<div class="hidden">
-	{#if data.transaction && data.transaction.trx}
-		{@const actions = data.transaction.trx.trx.actions as Action[]}
-		{#each actions as action}
-			{#if isValidAccount(action.account)}
-				<!-- {@const accountMap = summaryMap[action.account]} -->
-				{#if isValidActionName(action.account, action.name)}
-					<!-- {@const SummaryComponent = accountMap[action.name]} -->
-					<!-- <svelte:component this={SummaryComponent as typeof SvelteComponent} {action} /> -->
-				{:else}
-					<p>{action.account}::{action.name}</p>
-				{/if}
-			{:else}
-				<p>{action.account}</p>
-			{/if}
-			<Code>{JSON.stringify(action, null, 2)}</Code>
-		{/each}
-	{/if}
-
-	{#if data.seq}
-		{@const trace = data.transaction.traces.find(
-			(t: { receipt: { global_sequence: number } }) =>
-				String(t.receipt.global_sequence) === data.seq
-		)}
-		<Code>{JSON.stringify(trace, null, 2)}</Code>
-	{:else}
-		<Code>{JSON.stringify(data.transaction, null, 2)}</Code>
-	{/if}
-</div>
+<!-- <div class="hidden"> -->
+<!-- 	{#if data.transaction && data.transaction.trx} -->
+<!-- 		{@const actions = data.transaction.trx.trx.actions as Action[]} -->
+<!-- 		{#each actions as action} -->
+<!-- 			{#if isValidAccount(action.account)} -->
+<!-- {@const accountMap = summaryMap[action.account]} -->
+<!-- 				{#if isValidActionName(action.account, action.name)} -->
+<!-- {@const SummaryComponent = accountMap[action.name]} -->
+<!-- <svelte:component this={SummaryComponent as typeof SvelteComponent} {action} /> -->
+<!-- 				{:else} -->
+<!-- 					<p>{action.account}::{action.name}</p> -->
+<!-- 				{/if} -->
+<!-- 			{:else} -->
+<!-- 				<p>{action.account}</p> -->
+<!-- 			{/if} -->
+<!-- 			<Code>{JSON.stringify(action, null, 2)}</Code> -->
+<!-- 		{/each} -->
+<!-- 	{/if} -->
+<!-- 	{#if data.seq} -->
+<!-- 		{@const trace = data.transaction.traces.find( -->
+<!-- 			(t: { receipt: { global_sequence: number } }) => -->
+<!-- 				String(t.receipt.global_sequence) === data.seq -->
+<!-- 		)} -->
+<!-- 		<Code>{JSON.stringify(trace, null, 2)}</Code> -->
+<!-- 	{:else} -->
+<!-- 		<Code>{JSON.stringify(data.transaction, null, 2)}</Code> -->
+<!-- 	{/if} -->
+<!-- </div> -->
