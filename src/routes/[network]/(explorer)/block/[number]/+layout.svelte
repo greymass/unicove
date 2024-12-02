3a3640b938f02a3af970de0cce5e3ec463e328ff
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-12-02 12:33:55 -0800

    refactor: simpler current page check

--- src/routes/[network]/(explorer)/block/[number]/+layout.svelte
+++ src/routes/[network]/(explorer)/block/[number]/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { Stack } from '$lib/components/layout/index.js';
 	import PillGroup from '$lib/components/navigation/pillgroup.svelte';
 	import type { UnicoveContext } from '$lib/state/client.svelte.js';
@@ -9,23 +8,13 @@
 
 	const { settings } = getContext<UnicoveContext>('state');
 
-	const tabOptions = $derived.by(() => {
+	const options = $derived.by(() => {
 		let urlBase = `/${data.network}/block/${data.number}`;
 		return [
 			{ href: urlBase, text: 'Summary' },
 			{ href: `${urlBase}/data`, text: 'Data' }
 		];
 	});
-
-	let currentTab = $derived($page.url.pathname.split('/').slice(2)[3]);
-
-	// Derive the active state of each destination
-	let options = $derived(
-		tabOptions.map((option) => ({
-			...option,
-			active: option.href.split('/').slice(2)[2] === currentTab
-		}))
-	);
 </script>
 
 <Stack class="@container">
