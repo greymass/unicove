76f9fe03dd8d53a49d20ad6fc88328b8dec64147
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-12-02 12:33:55 -0800

    refactor: simpler current page check

--- src/routes/[network]/(account)/(send)/+layout.svelte
+++ src/routes/[network]/(account)/(send)/+layout.svelte
@@ -1,25 +1,15 @@
 <script lang="ts">
 	import PillGroup from '$lib/components/navigation/pillgroup.svelte';
-	import { page } from '$app/stores';
 
 	const { children, data } = $props();
 
-	const tabOptions = $derived.by(() => {
+	const options = $derived.by(() => {
 		const network = String(data.network);
 		return [
 			{ href: `/${network}/send`, text: 'Send' },
 			{ href: `/${network}/receive`, text: 'Receive' }
 		];
 	});
-
-	let currentTab = $derived($page.url.pathname.split('/')[3]);
-
-	let options = $derived(
-		tabOptions.map((option) => ({
-			...option,
-			active: option.href.split('/')[2] === currentTab
-		}))
-	);
 </script>
 
 <PillGroup {options} class="mb-6 hidden" />
