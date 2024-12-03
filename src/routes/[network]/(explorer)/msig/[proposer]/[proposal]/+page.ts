897b3a29be3168d837fd724ec1d4d2f739da33ad
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-12-02 17:24:11 -0800

    fix: type error

--- src/routes/[network]/(explorer)/msig/[proposer]/[proposal]/+page.ts
+++ src/routes/[network]/(explorer)/msig/[proposer]/[proposal]/+page.ts
@@ -1,15 +1,16 @@
 import { Name } from '@wharfkit/antelope';
 import type { PageLoad } from './$types';
+import { PermissionLevel } from '@wharfkit/antelope';
 
 export const load: PageLoad = async ({ parent }) => {
 	const { proposal } = await parent();
 
-	const requested_names: Name[] = proposal.approvals.requested_approvals.map(({ level }) =>
-		Name.from(level.actor)
+	const requested_names: Name[] = proposal.approvals.requested_approvals.map(
+		({ level }: { level: PermissionLevel }) => Name.from(level.actor)
 	);
 
-	const provided_names: Name[] = proposal.approvals.provided_approvals.map(({ level }) =>
-		Name.from(level.actor)
+	const provided_names: Name[] = proposal.approvals.provided_approvals.map(
+		({ level }: { level: PermissionLevel }) => Name.from(level.actor)
 	);
 
 	return {
