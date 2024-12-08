24c085560476c43eab67344a55196aea056486cc
Aaron Cox <aaron@greymass.com>
2024-12-07 18:29:36 -0800

    Balance alignment

--- src/routes/[network]/(explorer)/account/[name]/balances/+page.svelte
+++ src/routes/[network]/(explorer)/account/[name]/balances/+page.svelte
@@ -32,7 +32,8 @@
 		<thead>
 			<tr>
 				<th>Token</th>
-				<th>Amount</th>
+				<th class="text-right">Amount</th>
+				<th class="text-right">Value</th>
 				{#if isCurrentUser}
 					<th></th>
 				{/if}
@@ -55,9 +56,10 @@
 							</a>
 						</div>
 					</td>
-					<td>
+					<td class="text-right">
 						<AssetText value={balance.asset} />
 					</td>
+					<td></td>
 					{#if isCurrentUser}
 						{@render tableAction(balance.asset)}
 					{/if}
