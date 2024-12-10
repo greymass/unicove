31545d5ef1250a315247107d41febc9695a6d116
Aaron Cox <aaron@greymass.com>
2024-12-10 14:20:37 -0800

    Removed unneeded transact method

--- src/routes/[network]/(explorer)/msig/[proposer]/[proposal]/manager.svelte.ts
+++ src/routes/[network]/(explorer)/msig/[proposer]/[proposal]/manager.svelte.ts
@@ -91,23 +91,11 @@ export class ApprovalManager {
 		}
 	};
 
-	async transact(action: AnyAction) {
-		if (!this.wharf) {
-			throw new Error("Can't sign, wharf not ready");
-		}
-
-		const result = await this.wharf.transact({ action });
-
-		const txid = result.response?.transaction_id;
-		if (!txid) {
-			throw Error('No transaction id');
-		}
-	}
-
 	async approve() {
 		if (!this.network || !this.wharf || !this.wharf.session) {
 			throw new Error("Can't sign, data not ready");
 		}
+
 		const action = this.network.contracts.msig.action('approve', {
 			proposer: Name.from(this.proposal.proposer),
 			proposal_name: Name.from(this.proposal.name),
@@ -115,7 +103,7 @@ export class ApprovalManager {
 			proposal_hash: this.proposal.hash
 		});
 
-		await this.transact(action);
+		await this.wharf.transact({ action });
 		this.accountApprove(this.wharf.session.permissionLevel);
 	}
 
@@ -123,13 +111,14 @@ export class ApprovalManager {
 		if (!this.network || !this.wharf || !this.wharf.session) {
 			throw new Error("Can't sign, data not ready");
 		}
+
 		const action = this.network.contracts.msig.action('unapprove', {
 			proposer: Name.from(this.proposal.proposer),
 			proposal_name: Name.from(this.proposal.name),
 			level: this.wharf.session.permissionLevel
 		});
 
-		await this.transact(action);
+		await this.wharf.transact({ action });
 		this.accountUnapprove(this.wharf.session.permissionLevel);
 	}
 
@@ -137,26 +126,28 @@ export class ApprovalManager {
 		if (!this.network || !this.wharf || !this.wharf.session) {
 			throw new Error("Can't sign, data not ready");
 		}
+
 		const action = this.network.contracts.msig.action('exec', {
 			proposer: Name.from(this.proposal.proposer),
 			proposal_name: Name.from(this.proposal.name),
 			executer: this.wharf.session.actor
 		});
 
-		this.transact(action);
+		await this.wharf.transact({ action });
 	}
 
 	async cancel() {
 		if (!this.network || !this.wharf || !this.wharf.session) {
 			throw new Error("Can't sign, data not ready");
 		}
+
 		const action = this.network.contracts.msig.action('cancel', {
 			proposer: Name.from(this.proposal.proposer),
 			proposal_name: Name.from(this.proposal.name),
 			canceler: this.wharf.session.actor
 		});
 
-		this.transact(action);
+		await this.wharf.transact({ action });
 	}
 
 	toJSON() {
