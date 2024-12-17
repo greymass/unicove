c1d3511b2c6cdda6a48f64f803e019655b0df28a
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-12-17 15:06:27 -0800

    refactor: extract carousel to component

---
+++ src/routes/[network]/(homepage)/components/carousel.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import Metamask from '$lib/assets/metamask.svg';
+	import Box from '$lib/components/layout/box/box.svelte';
+	import { getContext } from 'svelte';
+	import type { UnicoveContext } from '$lib/state/client.svelte';
+	import TextBlock from './text-block.svelte';
+
+	const { network } = getContext<UnicoveContext>('state');
+
+	interface Props {
+		networkName: string;
+		networkLogo: string;
+	}
+
+	let { networkName, networkLogo }: Props = $props();
+</script>
+
+<section class="col-span-full @container">
+	<div class="grid min-h-72 rounded-2xl bg-mineShaft-950 px-4 @xl:grid-cols-2 @xl:gap-4">
+		<div class="grid place-items-center">
+			<svg
+				class="col-start-1 row-start-1 h-full w-full object-cover"
+				width="635"
+				height="296"
+				viewBox="0 0 635 296"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M201.577-49.292h231.851l115.925 200.789-115.925 200.788H201.577L85.652 151.497 201.577-49.292Z"
+					stroke="#fff"
+					stroke-opacity=".1"
+				/>
+				<path
+					d="M179.906 13.903 367.864-36.46l137.594 137.594-50.363 187.957-187.957 50.363L129.543 201.86l50.363-187.957Z"
+					stroke="#fff"
+					stroke-opacity=".5"
+				/>
+				<path
+					d="M179.009 72.29 317.506-7.424l138.497 79.712v159.422l-138.497 79.712-138.497-79.712V72.289Z"
+					stroke="#fff"
+				/>
+			</svg>
+			<div
+				class="col-start-1 row-start-1 grid max-w-sm grid-cols-3 items-center justify-items-center"
+			>
+				<img class="h-40" src={Metamask} alt="metamask" />
+				<svg width="36" height="36" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M18.008 0v36M36.008 18h-36" stroke="#fff" />
+				</svg>
+				<img
+					class="h-40 rounded-full bg-mineShaft-950 object-contain px-2 py-4"
+					src={networkLogo}
+					alt={networkName}
+				/>
+			</div>
+		</div>
+
+		<Box class="grid place-items-center py-8">
+			<TextBlock
+				{...{
+					title: `The EOS Wallet for MetaMask`,
+					text: `MetaMask, an the industry leading self-custody wallet, is now compatible with Unicove and the ${networkName} network. Install the ${networkName} Wallet snap for MetaMask to get started.`,
+					button: {
+						text: 'Install EOS Wallet for MetaMask',
+						href: `/${network}/metamask`
+					}
+				}}
+			/>
+		</Box>
+	</div>
+</section>
---
+++ src/routes/[network]/(homepage)/components/text-block.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Button from '$lib/components/button/button.svelte';
+	import Stack from '$lib/components/layout/stack.svelte';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		title: string;
+		text: string;
+		children?: Snippet;
+		button?: { text: string; href: string };
+	}
+	let props: Props = $props();
+</script>
+
+<Stack class="max-w-md items-start">
+	<h3 class="h3 leading-tight">{props.title}</h3>
+	<p>{props.text}</p>
+	{#if props.button && props.button.href}
+		<Button class="mt-1" href={props.button.href}>{props.button.text}</Button>
+	{/if}
+	{@render props.children?.()}
+</Stack>
