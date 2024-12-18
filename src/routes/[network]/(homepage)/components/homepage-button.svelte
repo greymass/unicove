6f0f59ffdd8fd79ca1899dbb441a712830cf03f4
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2024-12-17 16:40:31 -0800

    style: homepage button

---
+++ src/routes/[network]/(homepage)/components/homepage-button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Button from '$lib/components/button/button.svelte';
+	import { ArrowRight } from 'lucide-svelte';
+
+	interface Props {
+		text: string;
+		href: string;
+		icon?: boolean;
+	}
+	let props: Props = $props();
+</script>
+
+<Button class="-mt-2 bg-transparent px-0 text-solar-500" href={props.href}>
+	<span class="inline-flex items-center gap-1">
+		{props.text}
+
+		{#if props.icon}
+			<ArrowRight class="size-5" />
+		{/if}
+	</span>
+</Button>
--- src/routes/[network]/(homepage)/components/text-block.svelte
+++ src/routes/[network]/(homepage)/components/text-block.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Button from '$lib/components/button/button.svelte';
+	import Button from './homepage-button.svelte';
 	import Stack from '$lib/components/layout/stack.svelte';
 	import type { Snippet } from 'svelte';
 
@@ -16,7 +16,7 @@
 	<h3 class="h3 leading-tight">{props.title}</h3>
 	<p>{props.text}</p>
 	{#if props.button && props.button.href}
-		<Button class="mt-1" href={props.button.href}>{props.button.text}</Button>
+		<Button text={props.button.text} href={props.button.href} icon />
 	{/if}
 	{@render props.children?.()}
 </Stack>
