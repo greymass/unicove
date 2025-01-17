87860221cb692b05f5381c6121d9dd58075732ea
kurt <ttwishing@gmail.com>
2025-01-17 11:14:08 +0800

    check unlocalized strings

--- src/routes/[network]/(account)/resources/rex/+page.ts
+++ src/routes/[network]/(account)/resources/rex/+page.ts
@@ -1,9 +1,10 @@
 import { languageTag } from '$lib/paraglide/runtime';
 import type { PageLoad } from './$types';
+import * as m from '$lib/paraglide/messages';
 
 export const load: PageLoad = ({ params }) => {
 	return {
-		subtitle: 'Rent with Rex',
+		subtitle: m.resources_rent_with_rex_simple(),
 		backPath: `/${languageTag()}/${params.network}/resources`
 	};
 };
