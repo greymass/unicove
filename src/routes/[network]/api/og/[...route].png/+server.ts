b89bbdccd0a10407bbf5d8ba0ad5fe832bbea110
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-02-04 11:54:20 -0800

    refactor: use static image on no api

--- src/routes/[network]/api/og/[...route].png/+server.ts
+++ src/routes/[network]/api/og/[...route].png/+server.ts
@@ -3,35 +3,26 @@ import { PUBLIC_ENVIRONMENT } from '$env/static/public';
 import type { RequestHandler } from './$types';
 import { i18n } from '$lib/i18n';
 import { getCaption } from '$lib/utils/opengraph';
-import fs from 'fs';
-import path from 'path';
 
 export const GET: RequestHandler = async ({ url, fetch, params }) => {
 	const lang = i18n.getLanguageFromUrl(url);
 	const route = params.route;
 	const text = getCaption(route);
+	const cacheAge = PUBLIC_ENVIRONMENT === 'production' ? 86400 : 300;
+	let response: Response;
 
 	// Handle non-existant env variable
 	const API_OPENGRAPH_GENERATOR = env.API_OPENGRAPH_GENERATOR;
 
-	// Uses a local image if no API is provided
-	if (!API_OPENGRAPH_GENERATOR) {
-		const imagePath = path.resolve('static', 'opengraph', 'default.png');
-		const image = fs.readFileSync(imagePath);
-		return new Response(image, {
-			headers: {
-				'Content-Type': 'image/png'
-			}
-		});
+	if (API_OPENGRAPH_GENERATOR) {
+		const uri = new URL(API_OPENGRAPH_GENERATOR);
+		uri.searchParams.set('text', text);
+		uri.searchParams.set('lang', lang);
+		response = await fetch(uri.toString());
+	} else {
+		// Uses a local image if no API is provided
+		response = await fetch('/opengraph/default.png');
 	}
-
-	const uri = new URL(API_OPENGRAPH_GENERATOR);
-	uri.searchParams.set('text', text);
-	uri.searchParams.set('lang', lang);
-
-	const response = await fetch(uri.toString());
-
-	const cacheAge = PUBLIC_ENVIRONMENT === 'production' ? 86400 : 300;
 
 	return new Response(await response.arrayBuffer(), {
 		headers: {
