e8017f49e9c3f832cdb44bccc58668a08543bd39
Aaron Cox <aaron@greymass.com>
2025-02-16 10:06:34 -0800

    Added transport helpers for client/server type serialization

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,51 @@
-// file initialized by the Paraglide-SvelteKit CLI - Feel free to edit it
+import type { Transport } from '@sveltejs/kit';
+
 import { i18n } from '$lib/i18n';
+import { NetworkState, type SerializedNetworkState } from '$lib/state/network.svelte';
+import { AccountState, type SerializedAccountState } from '$lib/state/client/account.svelte';
+import { Name } from '@wharfkit/antelope';
 
 export const reroute = i18n.reroute();
+
+export const transport: Transport = {
+	AccountState: {
+		encode: (state) => {
+			if (state instanceof AccountState) {
+				return state.serialized;
+			}
+		},
+		decode: (serialized: SerializedAccountState) => {
+			const network = new NetworkState(serialized.network.config);
+			if (serialized.network.sources) {
+				network.setState(serialized.network.sources);
+			}
+			const state = new AccountState(network, serialized.name);
+			state.setState(serialized.sources);
+			return state;
+		}
+	},
+	Name: {
+		encode: (name) => {
+			if (name instanceof Name) {
+				return String(name);
+			}
+		},
+		decode: (name) => {
+			return Name.from(name);
+		}
+	},
+	NetworkState: {
+		encode: (state) => {
+			if (state instanceof NetworkState) {
+				return state.getState();
+			}
+		},
+		decode: (serialized: SerializedNetworkState) => {
+			const state = new NetworkState(serialized.config);
+			if (serialized.sources) {
+				state.setState(serialized.sources);
+			}
+			return state;
+		}
+	}
+};
