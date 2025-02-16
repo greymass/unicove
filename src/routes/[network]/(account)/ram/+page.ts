b6b5a65a44d8ee1f4bd1ddc46006779f1b93b291
Aaron Cox <aaron@greymass.com>
2025-02-16 10:08:18 -0800

    Prevent RAM page from attempting to load timeseries data

--- src/routes/[network]/(account)/ram/+page.ts
+++ src/routes/[network]/(account)/ram/+page.ts
@@ -12,20 +12,22 @@ export async function load({ fetch, parent }: LoadEvent): Promise<LoadData> {
 
 	let historicalPrices: HistoricalPrice[] = [];
 
-	try {
-		const response: Response = await fetch(`/${network}/api/metrics/marketprice/ram`);
-		const parsedResponse: { date: string; value: number }[] | { error: string } =
-			await response.json();
-		if ('error' in parsedResponse && parsedResponse.error) {
-			throw new Error(String(parsedResponse.error));
-		} else if (Array.isArray(parsedResponse)) {
-			historicalPrices = parsedResponse.map((price: { date: string; value: number }) => ({
-				date: new Date(price.date),
-				value: Asset.from(price.value / 10000, network.config.systemtoken.symbol || '0,UNKNOWN')
-			}));
+	if (network.supports('metrics')) {
+		try {
+			const response: Response = await fetch(`/${network}/api/metrics/marketprice/ram`);
+			const parsedResponse: { date: string; value: number }[] | { error: string } =
+				await response.json();
+			if ('error' in parsedResponse && parsedResponse.error) {
+				throw new Error(String(parsedResponse.error));
+			} else if (Array.isArray(parsedResponse)) {
+				historicalPrices = parsedResponse.map((price: { date: string; value: number }) => ({
+					date: new Date(price.date),
+					value: Asset.from(price.value / 10000, network.config.systemtoken.symbol || '0,UNKNOWN')
+				}));
+			}
+		} catch (error: unknown) {
+			console.error('Error fetching historical RAM prices:', error);
 		}
-	} catch (error: unknown) {
-		console.error('Error fetching historical RAM prices:', error);
 	}
 
 	return { historicalPrices };
