9c0cf6d4cbd07675fbd3da5de746de0cb7b1413d
Guras <sandro.guras@pm.me>
2025-02-20 00:07:20 +0200

    Cleanup
    * lint playwright file
    * remove test-example dir
    * remove playwright packages from package.json due to incompatibility with bun

--- tests/main-pages-visibility-test.spec.ts
+++ tests/main-pages-visibility-test.spec.ts
@@ -1,28 +1,34 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Login with test account.
-  await page.goto('https://dev.unicove.com/en/jungle4/debug/account');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByLabel('account-switcher-label').locator('div')).toContainText('wharfkit1133');
+	// Login with test account.
+	await page.goto('https://dev.unicove.com/en/jungle4/debug/account');
+	await page.getByRole('button', { name: 'Login' }).click();
+	await expect(page.getByLabel('account-switcher-label').locator('div')).toContainText(
+		'wharfkit1133'
+	);
 
-  // Go to the main page.
-  await page.getByRole('link', { name: 'My Account' }).click();
-  await expect(page.getByRole('heading', { name: 'wharfkit1133 Copy' }).locator('span')).toBeVisible();
+	// Go to the main page.
+	await page.getByRole('link', { name: 'My Account' }).click();
+	await expect(
+		page.getByRole('heading', { name: 'wharfkit1133 Copy' }).locator('span')
+	).toBeVisible();
 
-  // Go to the Send page.
-  await page.getByRole('navigation').getByRole('link', { name: 'Send' }).click();
-  await expect(page.getByRole('heading', { name: 'Send Tokens' }).locator('span')).toBeVisible();
+	// Go to the Send page.
+	await page.getByRole('navigation').getByRole('link', { name: 'Send' }).click();
+	await expect(page.getByRole('heading', { name: 'Send Tokens' }).locator('span')).toBeVisible();
 
-  // Go to the Staking page.
-  await page.getByRole('link', { name: 'Staking' }).click();
-  await expect(page.getByRole('heading', { name: 'Jungle 4 (Testnet) Network' }).locator('span')).toBeVisible();
+	// Go to the Staking page.
+	await page.getByRole('link', { name: 'Staking' }).click();
+	await expect(
+		page.getByRole('heading', { name: 'Jungle 4 (Testnet) Network' }).locator('span')
+	).toBeVisible();
 
-  // Go to the RAM page.
-  await page.getByRole('link', { name: 'RAM' }).click();
-  await expect(page.getByText('EOS/RAM Market', { exact: true })).toBeVisible();
+	// Go to the RAM page.
+	await page.getByRole('link', { name: 'RAM' }).click();
+	await expect(page.getByText('EOS/RAM Market', { exact: true })).toBeVisible();
 
-  // Go to the Settings page.
-  await page.getByRole('link', { name: 'Settings' }).click();
-  await expect(page.getByRole('heading', { name: 'Settings' }).locator('span')).toBeVisible();
+	// Go to the Settings page.
+	await page.getByRole('link', { name: 'Settings' }).click();
+	await expect(page.getByRole('heading', { name: 'Settings' }).locator('span')).toBeVisible();
 });
