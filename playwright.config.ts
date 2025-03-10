08b9e155bd45c96a27c3b52ed197ec2820a2f803
Sandro Guras <sandro.guras@pm.me>
2025-03-09 20:39:28 -0400

    tweak base url announcement below the dev server and add timeout according to docs

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,13 +24,6 @@ export default defineConfig({
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173',
-
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry'
-	},
 
 	/* Configure projects for major browsers */
 	projects: [
@@ -72,8 +65,16 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'bun run dev',
+		command: 'make dev',
 		url: 'http://localhost:5173',
-		reuseExistingServer: !process.env.CI
-	}
+		reuseExistingServer: !process.env.CI,
+		timeout: 10000,
+	},
+	use: {
+		/* Base URL to use in actions like `await page.goto('/')`. */
+		baseURL: 'http://localhost:5173',
+
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: 'on-first-retry'
+	},
 });
--- tests/main-pages-visibility-test.spec.ts
+++ tests/main-pages-visibility-test.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
 	// Login with test account.
-	await page.goto('/en/jungle4/debug/account');
+	await page.goto('./en/jungle4/debug/account');
 	await page.waitForLoadState('networkidle');
 	await page.getByRole('button', { name: 'Login' }).click();
 	await expect(page.getByLabel('account-switcher-label').locator('div')).toContainText(
