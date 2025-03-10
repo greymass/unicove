9a24ac8a74cf6e668fb7f3a27b7f82de200fe1a6
Aaron Cox <aaron@greymass.com>
2025-03-10 11:47:19 -0700

    Disable staking controls if staking is disabled in config

--- src/routes/[network]/(account)/staking/+page.svelte
+++ src/routes/[network]/(account)/staking/+page.svelte
@@ -80,96 +80,103 @@
 	</td>
 {/snippet}
 
-<MultiCard>
-	<Card id="account-value" style="column-span: all;">
-		<Cluster class="items-center">
-			<picture class="grid size-12 place-items-center rounded-full bg-mineShaft-900">
-				<ChartLine />
-			</picture>
-			<div>
-				<p>{m.common_apr_current()}</p>
-				<p class="text-2xl font-bold text-white">{apr}%</p>
-			</div>
-		</Cluster>
-	</Card>
-	<Card
-		id="token"
-		title={m.common_labeled_unit_balance({
-			unit: m.common_staking()
-		})}
-	>
-		<Stack>
-			<Stack class="gap-2">
-				<h4 class="text-muted text-base capitalize leading-none">
-					{m.common_labeled_unit_staked({
-						unit: m.common_tokens()
-					})}
-				</h4>
-				<p class="text-xl font-semibold leading-none text-white">
-					<AssetText variant="full" value={total} />
-				</p>
-				<Chip>
-					<AssetText variant="full" value={currencyValue} />
-					<!-- TODO: Percent change -->
-				</Chip>
-			</Stack>
-
-			{#if activity}
+{#if data.network.supports('staking')}
+	<MultiCard>
+		<Card id="account-value" style="column-span: all;">
+			<Cluster class="items-center">
+				<picture class="grid size-12 place-items-center rounded-full bg-mineShaft-900">
+					<ChartLine />
+				</picture>
+				<div>
+					<p>{m.common_apr_current()}</p>
+					<p class="text-2xl font-bold text-white">{apr}%</p>
+				</div>
+			</Cluster>
+		</Card>
+		<Card
+			id="token"
+			title={m.common_labeled_unit_balance({
+				unit: m.common_staking()
+			})}
+		>
+			<Stack>
 				<Stack class="gap-2">
-					<table class="table-styles text-muted">
-						<tbody>
-							{#if staked.units.gt(UInt64.from(0))}
-								<tr>
-									<td>{m.common_staked()}</td>
-									<td class="text-right text-white">
-										{#if staked.units.equals(0)}
-											<span class="font-mono">&lt;</span>
-										{/if}
-										<AssetText variant="full" value={staked} />
-										{#if context.settings.data.advancedMode}
-											<p>
-												<AssetText variant="full" value={stakedRex} />
-											</p>
-										{/if}
-									</td>
-									{@render tableAction([m.common_unstake(), `/${data.network}/staking/unstake`])}
-								</tr>
-							{/if}
-							{#if unstakingTotal.units.gt(UInt64.from(0))}
-								<tr>
-									<td>{m.common_unstaking()}</td>
-									<td class="text-right text-white">
-										<AssetText variant="full" value={unstakingTotal} />
-										{#if context.settings.data.advancedMode}
-											<p>
-												<AssetText variant="full" value={unstakingRex} />
-											</p>
-										{/if}
-									</td>
-									<td></td>
-								</tr>
-							{/if}
-							{#if totalWithdraw.units.gt(UInt64.from(0))}
-								<tr>
-									<td>{m.common_unstaked()}</td>
-									<td class="text-right text-white">
-										<AssetText variant="full" value={totalWithdraw} />
-										{#if context.settings.data.advancedMode}
-											<p>
-												<AssetText variant="full" value={unstakingRex} />
-											</p>
-										{/if}
-									</td>
-									{@render tableAction([m.common_withdraw(), `/${data.network}/staking/withdraw`])}
-								</tr>
-							{/if}
-						</tbody>
-					</table>
+					<h4 class="text-muted text-base capitalize leading-none">
+						{m.common_labeled_unit_staked({
+							unit: m.common_tokens()
+						})}
+					</h4>
+					<p class="text-xl font-semibold leading-none text-white">
+						<AssetText variant="full" value={total} />
+					</p>
+					<Chip>
+						<AssetText variant="full" value={currencyValue} />
+						<!-- TODO: Percent change -->
+					</Chip>
 				</Stack>
-			{/if}
-		</Stack>
-	</Card>
-	<AccountBalance cta={{ href: `/${networkName}/staking/stake`, label: m.common_stake() }} />
-	<UnstakingBalances records={unstaking} />
-	<StakingCalculator {apr} network={data.network} tokenprice={market.network.systemtoken.price} />
-</MultiCard>
+
+				{#if activity}
+					<Stack class="gap-2">
+						<table class="table-styles text-muted">
+							<tbody>
+								{#if staked.units.gt(UInt64.from(0))}
+									<tr>
+										<td>{m.common_staked()}</td>
+										<td class="text-right text-white">
+											{#if staked.units.equals(0)}
+												<span class="font-mono">&lt;</span>
+											{/if}
+											<AssetText variant="full" value={staked} />
+											{#if context.settings.data.advancedMode}
+												<p>
+													<AssetText variant="full" value={stakedRex} />
+												</p>
+											{/if}
+										</td>
+										{@render tableAction([m.common_unstake(), `/${data.network}/staking/unstake`])}
+									</tr>
+								{/if}
+								{#if unstakingTotal.units.gt(UInt64.from(0))}
+									<tr>
+										<td>{m.common_unstaking()}</td>
+										<td class="text-right text-white">
+											<AssetText variant="full" value={unstakingTotal} />
+											{#if context.settings.data.advancedMode}
+												<p>
+													<AssetText variant="full" value={unstakingRex} />
+												</p>
+											{/if}
+										</td>
+										<td></td>
+									</tr>
+								{/if}
+								{#if totalWithdraw.units.gt(UInt64.from(0))}
+									<tr>
+										<td>{m.common_unstaked()}</td>
+										<td class="text-right text-white">
+											<AssetText variant="full" value={totalWithdraw} />
+											{#if context.settings.data.advancedMode}
+												<p>
+													<AssetText variant="full" value={unstakingRex} />
+												</p>
+											{/if}
+										</td>
+										{@render tableAction([
+											m.common_withdraw(),
+											`/${data.network}/staking/withdraw`
+										])}
+									</tr>
+								{/if}
+							</tbody>
+						</table>
+					</Stack>
+				{/if}
+			</Stack>
+		</Card>
+		<AccountBalance cta={{ href: `/${networkName}/staking/stake`, label: m.common_stake() }} />
+		<UnstakingBalances records={unstaking} />
+		<StakingCalculator {apr} network={data.network} tokenprice={market.network.systemtoken.price} />
+	</MultiCard>
+{:else}
+	<p>This staking interface is not available on {data.network}.</p>
+{/if}
