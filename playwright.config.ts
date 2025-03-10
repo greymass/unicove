a4a3faa1602285a5bafeb32e9ec12520fe16cbbb
Sandro Guras <sandro.guras@pm.me>
2025-03-09 20:14:39 -0400

    rollback to original dev server setup in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,12 +68,13 @@ export default defineConfig({
 		//   name: 'Google Chrome',
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
-	]
+	],
 
 	/* Run your local dev server before starting the tests */
-	//webServer: {
-	//	command: 'bun run dev',
-	//	url: 'http://localhost:5173',
-	//	reuseExistingServer: !process.env.CI
-	//}
+	webServer: {
+		command: 'bun run dev',
+		url: 'http://localhost:5173',
+		reuseExistingServer: !process.env.CI,
+		timeout: 3000
+	}
 });
