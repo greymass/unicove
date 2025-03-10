b452f3a22ec2e777dfaae677d26dea4bb1b8f4f8
Sandro Guras <sandro.guras@pm.me>
2025-03-10 16:52:21 -0400

    add timeout to first expect assertion

--- tests/main-pages-visibility-test.spec.ts
+++ tests/main-pages-visibility-test.spec.ts
@@ -4,9 +4,10 @@ test('test', async ({ page }) => {
 	// Login with test account.
 	await page.goto('./en/jungle4/debug/account');
 	await page.waitForLoadState('networkidle');
-	await page.getByRole('button', { name: 'Login' }).click({ force: true }) ;
+	await page.getByRole('button', { name: 'Login' }).click({ force: true });
 	await expect(page.getByLabel('account-switcher-label').locator('div')).toContainText(
-		'wharfkit1133'
+		'wharfkit1133',
+		{ timeout: 10 * 1000 }
 	);
 
 	// Go to the main page.
