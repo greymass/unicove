04fbb343ac072ae37ecd17e967fb6f24ce4f370a
Sandro Guras <sandro.guras@pm.me>
2025-03-10 09:25:13 -0400

    change url to localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,14 +66,14 @@ export default defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'make dev',
-		url: 'http://10.1.0.60:5173/',
+		url: 'http://localhost:5173',
 		reuseExistingServer: true,
 		timeout: 25 * 1000,
 		stdout: 'pipe',
 	},
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://10.1.0.60:5173/',
+		baseURL: 'http://localhost:5173',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry'
