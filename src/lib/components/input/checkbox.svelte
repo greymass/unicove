2f91a1bc388df92752ada815bd07b6af404a73ad
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-04-03 16:03:51 -0700

    refactor: use theme color roles

--- src/lib/components/input/checkbox.svelte
+++ src/lib/components/input/checkbox.svelte
@@ -43,34 +43,7 @@
 <div class="flex items-center">
 	<button
 		use:melt={$root}
-		class="
-			border-mine-700
-			focus-visible:border-solar-500
-			focus-visible:outline-solar-500
-			disabled:border-mine-950
-			disabled:bg-mine-950
-			data-[state=checked]:disabled:border-mine-950
-			data-[state=checked]:disabled:bg-mine-950
-			flex
-			size-5
-			appearance-none
-		items-center
-			justify-center
-			rounded
-			border
-			border-solid
-			bg-transparent
-			text-sky-950
-		focus-visible:outline
-		focus-visible:outline-2
-			focus-visible:outline-offset-[-2px]
-			disabled:cursor-not-allowed
-			disabled:text-white/30
-			data-[state=checked]:border-sky-500
-			data-[state=checked]:bg-sky-500
-			data-[state=checked]:hover:border-sky-400
-			data-[state=checked]:hover:bg-sky-400
-			"
+		class=" border-mine-700 focus-visible:border-solar-500 focus-visible:outline-solar-500 disabled:border-mine-950 disabled:bg-mine-950 data-[state=checked]:disabled:border-mine-950 data-[state=checked]:disabled:bg-mine-950 text-on-primary data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:hover:border-primary data-[state=checked]:hover:bg-primary/90 flex size-5 cursor-pointer appearance-none items-center justify-center rounded border border-solid bg-transparent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-[-2px] disabled:cursor-not-allowed disabled:text-white/30"
 		{id}
 		aria-labelledby={ariaLabelledBy}
 	>
