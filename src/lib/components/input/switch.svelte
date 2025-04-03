b83456c403cfa532795021ff6ab856caa6fab1de
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-04-03 16:03:51 -0700

    refactor: use theme color roles

--- src/lib/components/input/switch.svelte
+++ src/lib/components/input/switch.svelte
@@ -44,35 +44,9 @@
 
 <div class="flex items-center">
 	<button
-		use:melt={$root}
-		class="
-		border-mine-600
-		*:bg-mine-400
-		focus-visible:border-solar-500
-		focus-visible:outline-solar-500
-		relative
-		h-8
-		w-[52px]
-		rounded-full
-		border-2
-		bg-transparent
-		*:size-4
-		*:translate-x-1.5
-		*:transition-transform
-		focus-visible:outline
-		focus-visible:outline-2
-		focus-visible:outline-offset-[-2px]
-		disabled:cursor-not-allowed
-		disabled:opacity-40
-		data-[state=checked]:border-sky-500
-		data-[state=checked]:bg-sky-500
-		data-[state=checked]:*:translate-x-[26px]
-		data-[state=checked]:*:scale-150
-		data-[state=checked]:*:bg-sky-50
-		data-[state=checked]:hover:border-sky-400
-		data-[state=checked]:hover:bg-sky-400
-"
 		{id}
+		use:melt={$root}
+		class=" border-mine-600 *:bg-mine-400 focus-visible:border-solar-500 focus-visible:outline-solar-500 data-[state=checked]:*:bg-on-primary-container data-[state=checked]:bg-primary-container data-[state=checked]:border-primary-container data-[state=checked]:hover:bg-primary-container/90 data-[state=checked]:hover:border-primary-container/90 relative h-8 w-[52px] rounded-full border-2 bg-transparent *:size-4 *:translate-x-1.5 *:transition-transform focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-[-2px] disabled:cursor-not-allowed disabled:opacity-40 data-[state=checked]:*:translate-x-[26px] data-[state=checked]:*:scale-150"
 		aria-labelledby={ariaLabelledBy}
 	>
 		<span class="thumb block rounded-full"></span>
