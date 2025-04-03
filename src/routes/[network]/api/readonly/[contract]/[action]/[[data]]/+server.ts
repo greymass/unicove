877695edc7b1ba21d04f3bc764f2d33ee7b45e1d
Aaron Cox <aaron@greymass.com>
2025-04-03 11:13:13 -0700

    Determine if the decoded type is stringable, and if so just return text

--- src/routes/[network]/api/readonly/[contract]/[action]/[[data]]/+server.ts
+++ src/routes/[network]/api/readonly/[contract]/[action]/[[data]]/+server.ts
@@ -1,8 +1,9 @@
-import { json } from '@sveltejs/kit';
+import { json, text } from '@sveltejs/kit';
 
 import { ContractKit } from '@wharfkit/contract';
 import { Name, Serializer, Transaction } from '@wharfkit/antelope';
 import type { RequestEvent } from './$types';
+import { stringableTypes } from '$lib/types/string';
 
 export async function GET({ locals: { network }, params }: RequestEvent) {
 	const contractKit = new ContractKit({
@@ -33,12 +34,15 @@ export async function GET({ locals: { network }, params }: RequestEvent) {
 		throw new Error(`Return type for ${name} not defined in the ABI.`);
 	}
 
-	return json(
-		Serializer.decode({
-			data: hexData,
-			type: returnType.result_type,
-			abi: contract.abi
-		}),
-		{ status: 200 }
-	);
+	const decoded = Serializer.decode({
+		data: hexData,
+		type: returnType.result_type,
+		abi: contract.abi
+	});
+
+	if (stringableTypes.includes(returnType.result_type)) {
+		return text(String(decoded), { status: 200 });
+	} else {
+		return json(decoded, { status: 200 });
+	}
 }
