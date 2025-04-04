95530012dce66f35658d466addfd07e23e9c1272
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-04-04 16:34:11 -0700

    refactor: color roles in select input

--- src/lib/components/select/elements/menu.svelte
+++ src/lib/components/select/elements/menu.svelte
@@ -12,20 +12,24 @@
 		menu: AnyMeltElement;
 		class?: string;
 	}
+	import { getContext } from 'svelte';
+	import type { UnicoveContext } from '$lib/state/client.svelte';
+
+	const context = getContext<UnicoveContext>('state');
 
 	const { class: className, menu, ...props }: Props = $props();
 </script>
 
 {#if props.open}
 	<div
+		data-theme={context.network}
+		data-variant={props.variant}
 		class={cn(
 			'border-mine-600 bg-shark-950 z-90 flex max-h-[300px] flex-col overflow-y-auto border-2 py-1 shadow-sm focus:ring-0! data-[variant=form]:rounded-lg data-[variant=form]:px-2 data-[variant=form]:py-2 data-[variant=pill]:rounded-2xl data-[variant=pill]:px-1 data-[variant=pill]:py-1 ',
 			className
 		)}
-		data-variant={props.variant}
 		use:melt={$menu}
 		transition:fade={{ duration: 100 }}
-		id={props.id + '-menu'}
 	>
 		{@render props.children()}
 	</div>
