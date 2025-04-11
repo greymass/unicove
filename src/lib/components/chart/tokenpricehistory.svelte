cdedf5de85620351e147d7da713aa63ac7238c27
Aaron Cox <aaron@greymass.com>
2025-04-10 19:21:05 -0700

    Updating token history to convert to preferred currency

--- src/lib/components/chart/tokenpricehistory.svelte
+++ src/lib/components/chart/tokenpricehistory.svelte
@@ -5,27 +5,44 @@
 	import type { MarketContext, UnicoveContext } from '$lib/state/client.svelte';
 	import type { HistoricalPrice } from '$lib/types';
 	import Loading from './loading.svelte';
+	import { Currencies } from '$lib/types/currencies';
+	import { calculateValue } from '$lib/utils';
 
-	const { network } = getContext<UnicoveContext>('state');
+	const context = getContext<UnicoveContext>('state');
 	const market = getContext<MarketContext>('market');
 
 	type Price = { date: string; value: number };
 	type APIResponse = Price[] | { error: string };
 
 	const fetchTokenPrices = async () => {
-		if (network.supports('timeseries')) {
+		if (context.network.supports('timeseries')) {
 			try {
-				const response = await network.fetch(`/${network}/api/metrics/marketprice/token`);
+				const response = await context.network.fetch(
+					`/${context.network}/api/metrics/marketprice/token`
+				);
 				const parsedTokenResponse: APIResponse = await response.json();
 
 				if (Array.isArray(parsedTokenResponse) && parsedTokenResponse.length) {
-					return parsedTokenResponse.map(
-						(price: Price) =>
-							({
-								date: new Date(price.date),
-								value: Asset.from(price.value / 10000, '4,USD')
-							}) as HistoricalPrice
-					);
+					const currency = Currencies[context.settings.data.displayCurrency];
+					const pair = market.market.getPair(Currencies.USD, currency);
+					const converted = parsedTokenResponse.map((price: Price): HistoricalPrice => {
+						let value = Asset.from(price.value / 10000, '4,USD');
+						if (pair) {
+							value = calculateValue(value, pair.price);
+						}
+						return {
+							date: new Date(price.date),
+							value
+						};
+					});
+					const tokenpair = market.market.getPair(context.network.token.id, currency);
+					if (tokenpair) {
+						converted.unshift({
+							date: new Date(),
+							value: tokenpair.price
+						});
+					}
+					return converted;
 				} else if ('error' in parsedTokenResponse && parsedTokenResponse.error) {
 					throw new Error(String(parsedTokenResponse.error));
 				} else {
@@ -39,18 +56,15 @@
 		}
 	};
 
-	let systemtoken = Asset.Symbol.from(network.config.systemtoken.symbol);
-	let pair = $derived(String(systemtoken.name) + '/USD');
+	let systemtoken = Asset.Symbol.from(context.network.config.systemtoken.symbol);
+	let currency = $derived(Currencies[context.settings.data.displayCurrency]);
+	let pair = $derived(String(systemtoken.name) + '/' + currency.symbol.name);
 </script>
 
 {#await fetchTokenPrices()}
 	<Loading {pair} />
 {:then prices}
 	{#if prices.length}
-		<ChartContainer
-			{pair}
-			data={[{ date: new Date(), value: market.network.systemtoken.price }, ...prices]}
-			type="line"
-		/>
+		<ChartContainer {pair} data={prices} type="line" />
 	{/if}
 {/await}
