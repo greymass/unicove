32356f99335a32a9007ed1aa41d2bdd649628b01
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-04-28 12:02:48 -0700

    fix: copy button feedback now uses icon

--- src/lib/components/button/copy.svelte
+++ src/lib/components/button/copy.svelte
@@ -4,6 +4,7 @@
 	import type { UnicoveContext } from '$lib/state/client.svelte';
 	import { getContext, type ComponentProps } from 'svelte';
 	import IconButton from '$lib/components/button/icon.svelte';
+	import { ClipboardCheck } from 'lucide-svelte';
 
 	const context = getContext<UnicoveContext>('state');
 
@@ -20,7 +21,7 @@
 			await navigator.clipboard.writeText(props.data);
 			if (context.settings.data.debugMode) console.info(props.data, 'copied to clipboard');
 			hint = true;
-			setTimeout(() => (hint = false), 300);
+			setTimeout(() => (hint = false), 500);
 		} catch (err) {
 			if (context.settings.data.debugMode) console.error('Failed to copy text: ', err);
 		}
@@ -28,13 +29,9 @@
 </script>
 
 {#if browser && 'clipboard' in navigator}
-	<IconButton icon={Copy} onclick={copyToClipboard} {...props} />
 	{#if hint}
-		<!-- 		<span -->
-		<!-- 			in:fly={{ x: -20, easing: quartOut, duration: 100 }} -->
-		<!-- 			out:fade={{ easing: quadIn, duration: 200 }} -->
-		<!-- 			class="text-primary absolute inset-y-0 left-full translate-x-2 text-xs text-nowrap select-none" -->
-		<!-- 			>{m.common_copied_result()}</span -->
-		<!-- 		> -->
+		<IconButton icon={ClipboardCheck} {...props} />
+	{:else}
+		<IconButton icon={Copy} onclick={copyToClipboard} {...props} />
 	{/if}
 {/if}
