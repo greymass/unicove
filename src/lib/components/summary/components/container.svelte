b6cb127c564d10195644769a84261e6c14c3618e
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-05-06 15:27:06 -0700

    fix: action table overflow

--- src/lib/components/summary/components/container.svelte
+++ src/lib/components/summary/components/container.svelte
@@ -2,6 +2,8 @@
 	let props = $props();
 </script>
 
-<div class="table w-fit border-spacing-4">
-	{@render props.children()}
+<div class="overflow-x-auto">
+	<div class="table w-fit border-spacing-4">
+		{@render props.children()}
+	</div>
 </div>
