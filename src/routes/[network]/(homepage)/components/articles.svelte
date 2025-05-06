bf22d37bdf0fb5eae46ec4910d90010c58d77277
Aaron Cox <aaron@greymass.com>
2025-05-06 13:04:04 -0700

    Wrapping article in key

--- src/routes/[network]/(homepage)/components/articles.svelte
+++ src/routes/[network]/(homepage)/components/articles.svelte
@@ -19,52 +19,54 @@
 
 <section class="@container col-span-full grid *:col-span-full *:col-start-1 *:row-start-1">
 	{#each articles as article, i}
-		<div
-			class:sr-only={i !== index}
-			class="grid min-h-72 grid-cols-2 items-center gap-8 rounded-2xl @3xl:grid-cols-[auto_1fr_1fr_auto] @3xl:gap-y-10"
-		>
-			<IconButton
-				icon={ChevronLeft}
-				size="large"
-				class="text-muted col-start-1  row-start-3 @3xl:row-start-1"
-				onclick={onPrevious}
-			/>
-			<div class="col-span-full grid place-items-center @3xl:col-span-1">
-				<img
-					src={article.thumbnail}
-					alt={article.title}
-					class="rounded-2xl"
-					width="512"
-					height="288"
-				/>
-			</div>
-			<div class="col-span-full grid place-items-center @3xl:col-span-1">
-				<TextBlock title={article.title} text={article.description}>
-					<Button variant="primary" href={article.slug}>
-						{m.common_read_more()}
-					</Button>
-				</TextBlock>
-			</div>
-			<IconButton
-				icon={ChevronRight}
-				size="large"
-				class="text-muted col-start-2 row-start-3 justify-self-end @3xl:col-start-4 @3xl:row-start-1"
-				onclick={onNext}
-			/>
-
+		{#key article.slug}
 			<div
-				class="col-span-full col-start-1 row-start-3 flex items-center justify-center gap-2 @3xl:row-start-2"
+				class:sr-only={i !== index}
+				class="grid min-h-72 grid-cols-2 items-center gap-8 rounded-2xl @3xl:grid-cols-[auto_1fr_1fr_auto] @3xl:gap-y-10"
 			>
-				{#each articles as article, i}
-					<button onclick={() => (index = i)}>
-						<Circle
-							id={article.slug}
-							aria-selected={i === index}
-							class="stroke-on-background aria-selected:fill-on-background size-2"
-						/>
-					</button>
-				{/each}
+				<IconButton
+					icon={ChevronLeft}
+					size="large"
+					class="text-muted col-start-1  row-start-3 @3xl:row-start-1"
+					onclick={onPrevious}
+				/>
+				<div class="col-span-full grid place-items-center @3xl:col-span-1">
+					<img
+						src={article.thumbnail}
+						alt={article.title}
+						class="rounded-2xl"
+						width="512"
+						height="288"
+					/>
+				</div>
+				<div class="col-span-full grid place-items-center @3xl:col-span-1">
+					<TextBlock title={article.title} text={article.description}>
+						<Button variant="primary" href={article.slug}>
+							{m.common_read_more()}
+						</Button>
+					</TextBlock>
+				</div>
+				<IconButton
+					icon={ChevronRight}
+					size="large"
+					class="text-muted col-start-2 row-start-3 justify-self-end @3xl:col-start-4 @3xl:row-start-1"
+					onclick={onNext}
+				/>
+
+				<div
+					class="col-span-full col-start-1 row-start-3 flex items-center justify-center gap-2 @3xl:row-start-2"
+				>
+					{#each articles as article, i}
+						<button onclick={() => (index = i)}>
+							<Circle
+								id={article.slug}
+								aria-selected={i === index}
+								class="stroke-on-background aria-selected:fill-on-background size-2"
+							/>
+						</button>
+					{/each}
+				</div>
 			</div>
-		</div>
+		{/key}
 	{/each}
 </section>
