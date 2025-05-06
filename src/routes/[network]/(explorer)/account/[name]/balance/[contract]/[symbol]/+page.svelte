b09179b8e773221617f19e034c593a028d14bf70
Aaron Cox <aaron@greymass.com>
2025-05-05 17:33:05 -0700

    Linting on placeholder page

--- src/routes/[network]/(explorer)/account/[name]/balance/[contract]/[symbol]/+page.svelte
+++ src/routes/[network]/(explorer)/account/[name]/balance/[contract]/[symbol]/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-	import Button from '$lib/components/button/button.svelte';
 	import Code from '$lib/components/code.svelte';
-	import Account from '$lib/components/elements/account.svelte';
-	import AssetText from '$lib/components/elements/asset.svelte';
 	import type { UnicoveContext } from '$lib/state/client.svelte.js';
 	import { getContext } from 'svelte';
 
-	const context = getContext<UnicoveContext>('state');
 	const props = $props();
 </script>
 
