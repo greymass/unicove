9e5fb49904c37b142937d8723616bd61ab53e515
Aaron Cox <aaron@greymass.com>
2025-05-07 11:45:48 -0700

    Added delphioracle write

---
+++ src/lib/components/summary/delphioracle/write.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as DelphiContract from '$lib/wharf/contracts/delphioracle';
+	import Chip from '$lib/components/chip.svelte';
+	import type { ActionSummaryProps } from '$lib/types/transaction';
+
+	interface Props extends Omit<ActionSummaryProps, 'data'> {
+		data: DelphiContract.Types.write;
+	}
+
+	const { data }: Props = $props();
+</script>
+
+<div class="space-y-2">
+	{#each data.quotes as quote}
+		<div class="flex gap-2">
+			<Chip>{quote.pair}</Chip>
+			{quote.value}
+		</div>
+	{/each}
+</div>
