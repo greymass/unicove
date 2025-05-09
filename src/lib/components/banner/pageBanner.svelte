7b2f8f9f1ed179f537c61efa5d76045c2b50039f
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-05-09 14:15:33 -0700

    fix: banner text color

--- src/lib/components/banner/pageBanner.svelte
+++ src/lib/components/banner/pageBanner.svelte
@@ -41,7 +41,7 @@
 
 {#if showBanner}
 	<aside
-		class="grid grid-cols-[auto_1fr_auto] items-center justify-items-center gap-4 bg-linear-to-r from-[#1C2399] to-[#2E3BFF] text-white shadow-lg *:row-start-1"
+		class="text-white grid grid-cols-[auto_1fr_auto] items-center justify-items-center gap-4 bg-linear-to-r from-[#1C2399] to-[#2E3BFF] shadow-lg *:row-start-1"
 	>
 		<a
 			class="col-start-2 py-4 text-white underline underline-offset-4 md:col-span-3 md:col-start-1 md:text-center"
