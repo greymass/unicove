4bbb4c030cd7c8e1a2053b4ad204fccd6c226a0a
Aaron Cox <aaron@greymass.com>
2025-05-09 14:29:10 -0700

    Changed exchange icon

--- src/lib/components/tags/account.svelte
+++ src/lib/components/tags/account.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { FileBadge2, BadgeCheck, BadgeHelp } from 'lucide-svelte';
+	import { FileBadge2, BadgeCheck, BadgeHelp, BadgeDollarSign } from 'lucide-svelte';
 	import Tooltip from '$lib/components/tooltip/tooltip.svelte';
 	import type { NetworkState } from '$lib/state/network.svelte';
 	import * as m from '$lib/paraglide/messages';
@@ -29,8 +29,8 @@
 				<BadgeCheck class={cn('text-solar-500', props.class)} />
 			</Tooltip>
 		{:else if tag.tag.equals('exchange')}
-			<Tooltip content={m.tooltip_exchange_account()} icon={BadgeCheck}>
-				<BadgeCheck class={cn('text-solar-500', props.class)} />
+			<Tooltip content={m.tooltip_exchange_account()} icon={BadgeDollarSign}>
+				<BadgeDollarSign class={cn('text-solar-500', props.class)} />
 			</Tooltip>
 		{:else}
 			<Tooltip content={m.tooltip_tagged_account({ tag: tag.tag })} icon={BadgeHelp}>
