1050fb982e4e7c794dc63c0ba1bc8b303ad0c73a
Aaron Cox <aaron@greymass.com>
2025-05-10 13:21:30 -0700

    Conditionally format `quant` element since Hyperion uses a float?

--- src/lib/components/summary/eosio/buyram.svelte
+++ src/lib/components/summary/eosio/buyram.svelte
@@ -6,16 +6,29 @@
 	import AccountElement from '$lib/components/elements/account.svelte';
 	import AssetElement from '$lib/components/elements/asset.svelte';
 	import Row from '../components/row.svelte';
+	import { systemtoken } from '$lib/wharf/chains';
 
 	interface Props extends Omit<ActionSummaryProps, 'data'> {
 		data: SystemContract.Types.buyram;
 	}
 
 	const { data }: Props = $props();
+	const quant = $derived.by(() => {
+		try {
+			return Asset.from(data.quant);
+		} catch {
+			try {
+				return Asset.fromUnits(data.quant, systemtoken.symbol);
+			} catch (error) {
+				console.error('Error parsing quant:', error);
+			}
+		}
+		return Asset.fromUnits(0, systemtoken.symbol);
+	});
 </script>
 
 <Row>
-	<AssetElement value={Asset.from(data.quant)} variant="full" />
+	<AssetElement value={quant} variant="full" />
 	<AccountElement name={Name.from(data.payer)} />
 	{#if !Name.from(data.receiver).equals(data.payer)}
 		<span>
