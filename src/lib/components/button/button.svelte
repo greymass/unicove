3f8307f5c6b092fb4b8395f427149383d345c241
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-05-14 11:18:56 -0700

    style: filled secondary buttons

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -46,10 +46,13 @@
 	);
 
 	const filledStyles =
-		' inline-flex h-12 grow  rounded-lg bg-primary px-8  text-on-primary  focus:outline-transparent focus-visible:outline focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-solar-500 disabled:bg-on-surface/12 disabled:text-on-surface/40';
+		' inline-flex h-12 grow  rounded-lg bg-primary px-8  text-on-primary  focus:outline-transparent focus-visible:outline focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-solar-500 disabled:bg-on-surface/12 disabled:text-on-surface/60';
+
+	const secondaryFilledStyles =
+		'inline-flex h-12 grow rounded-lg bg-primary/10 dark:bg-primary-container/30 px-8 text-primary focus:outline-transparent focus-visible:outline focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-solar-500 disabled:bg-on-surface/12 disabled:text-on-surface/60';
 
 	const outlinedStyles =
-		' flex h-12 grow  rounded-lg px-8  text-primary ring-2 ring-inset ring-outline-variant   focus-visible:outline-hidden focus-visible:ring-solar-500  hover:active:ring-outline-variant  disabled:text-on-surface/40  disabled:hover:bg-transparent disabled:hover:ring-on-surface/12 disabled:ring-on-surface/12  ';
+		' flex h-12 grow rounded-lg px-8  text-primary ring-2 ring-inset ring-outline-variant   focus-visible:outline-hidden focus-visible:ring-solar-500  hover:active:ring-outline-variant  disabled:text-on-surface/40  disabled:hover:bg-transparent disabled:hover:ring-on-surface/12 disabled:ring-on-surface/12  ';
 
 	const pillStyles =
 		' inline-flex h-10  rounded-full border-2 border-transparent px-5  leading-4  focus-visible:border-solar-500  focus-visible:outline-hidden   aria-[current]:border-outline-variant aria-[current]:focus-visible:border-solar-500';
@@ -59,7 +62,8 @@
 
 	let styles = {
 		primary: filledStyles,
-		secondary: outlinedStyles,
+		outlined: outlinedStyles,
+		secondary: secondaryFilledStyles,
 		pill: pillStyles,
 		tertiary: textStyles
 	};
