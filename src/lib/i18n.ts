2c7e6e4e6405af881f0381ec43004a9758befa6d
aaroncox <aaron.cox@greymass.com>
2025-10-21 00:02:43 -0700

    Removed paraglide

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,11 +1,4 @@
-import { createI18n } from '@inlang/paraglide-sveltekit';
-import * as runtime from './paraglide/runtime';
 import type { Asset } from '@wharfkit/antelope';
-
-// export const i18n = createI18n(runtime, {
-// 	prefixDefaultLanguage: 'always',
-// 	exclude: [/^\/[a-z0-9]+\/api\/.*/]
-// });
 
 export const languageNames = {
 	en: 'English',
