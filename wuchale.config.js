b47f9aa27cc094445d95c6a507191e65d2cd70e7
aaroncox <aaron.cox@greymass.com>
2025-10-21 18:51:49 -0700

    Moved translation module to be optional instead of comment

--- wuchale.config.js
+++ wuchale.config.js
@@ -2,7 +2,14 @@ import { adapter as svelte } from '@wuchale/svelte';
 import { adapter as js } from 'wuchale/adapter-vanilla';
 import { defineConfig, gemini } from 'wuchale';
 
-// const ignore = ['src/routes/[network]/(dev)/**'];
+let ai;
+if (process.env.WUCHALE_GEMINI === 'true') {
+	ai = gemini({
+		batchSize: 40,
+		parallel: 5,
+		think: true // default: false
+	});
+}
 
 export default defineConfig({
 	otherLocales: ['ko', 'zh'],
@@ -17,10 +24,6 @@ export default defineConfig({
 			// 	proxy: true
 			// }
 		})
-	}
-	// ai: gemini({
-	// 	batchSize: 40,
-	// 	parallel: 5,
-	// 	think: true // default: false
-	// })
+	},
+	ai
 });
