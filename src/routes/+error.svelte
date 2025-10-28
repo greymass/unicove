575a2b5abe510a0fbb3f97f6f909c2ba98b63269
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-10-27 18:41:21 -0700

    Fix for no theme on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,22 @@
 	import { Button } from 'unicove-components';
 </script>
 
+<svelte:head>
+	<!-- @wc-ignore -->
+	<script>
+		(function () {
+			if (typeof window !== undefined) {
+				const storedTheme = localStorage.getItem('color-scheme');
+				const theme =
+					storedTheme ||
+					(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+				document.documentElement.setAttribute('data-scheme', theme);
+				if (!storedTheme) localStorage.setItem('color-scheme', theme);
+			}
+		})();
+	</script>
+</svelte:head>
+
 <div class="mx-auto max-w-prose">
 	<Stack class="py-8 text-center">
 		<div class=" *:object-fit mx-auto max-w-prose *:size-full">
@@ -18,6 +34,7 @@
 			{/if}
 		</h1>
 
+		<!-- @wc-ignore -->
 		<Button class="mx-auto max-w-fit" variant="primary" data-sveltekit-reload href="/">
 			Go to Unicove
 		</Button>
