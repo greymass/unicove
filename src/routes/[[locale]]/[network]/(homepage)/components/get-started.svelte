f204691119620d73c9d9a36088e83c0795258f60
dafuga <danielfugere28@gmail.com>
2025-11-24 20:00:08 -0800

    refactor: using effect instead of polling

--- src/routes/[[locale]]/[network]/(homepage)/components/get-started.svelte
+++ src/routes/[[locale]]/[network]/(homepage)/components/get-started.svelte
@@ -8,35 +8,23 @@
 	const context = getContext<UnicoveContext>('state');
 	const urlPath = $derived(context.urlPath);
 
-	let pluginAvailable = $state(true);
 	const TARGET_PLUGIN_ID = 'web-authenticator';
 
-	function checkAvailability() {
-		const sessionKit = context.wharf.sessionKit;
-		if (!sessionKit) {
-			return false;
-		}
+	let pluginAvailable = $state(true);
 
-		const walletPlugins = sessionKit.walletPlugins;
-		const hasPlugin = walletPlugins.some((wallet) => wallet.id === TARGET_PLUGIN_ID);
-		pluginAvailable = hasPlugin;
-		return hasPlugin;
+	function checkPluginAvailability() {
+		const sessionKit = context.wharf.sessionKit;
+		if (sessionKit) {
+			pluginAvailable = sessionKit.walletPlugins.some((wallet) => wallet.id === TARGET_PLUGIN_ID);
+		}
 	}
 
+	// Track chain reactively - when it's set, sessionKit should be available
 	$effect(() => {
-		if (checkAvailability()) {
-			return;
+		const chain = context.wharf.chain; // Track chain changes
+		if (chain) {
+			checkPluginAvailability();
 		}
-
-		const interval = setInterval(() => {
-			if (checkAvailability()) {
-				clearInterval(interval);
-			}
-		}, 250);
-
-		return () => {
-			clearInterval(interval);
-		};
 	});
 
 	async function handleGetStarted() {
