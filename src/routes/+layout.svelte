190a2664f17683b5c84dd3cf7fdf8e7ee469dce0
aaroncox <aaron.cox@greymass.com>
2026-03-11 10:09:13 -0700

    Remove plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import '$lib/utils/dayjs'; // setup dayjs
-	import { PlausibleAnalytics } from '@accuser/svelte-plausible-analytics';
-	import { PUBLIC_ENVIRONMENT, PUBLIC_ANALYTICS_DOMAIN } from '$env/static/public';
+	import { PUBLIC_ENVIRONMENT } from '$env/static/public';
 
 	import Toaster from '$lib/components/toast/toaster.svelte';
 
@@ -18,7 +17,3 @@
 <Toaster />
 
 {@render children?.()}
-
-{#if PUBLIC_ENVIRONMENT === 'production'}
-	<PlausibleAnalytics apiHost="https://stats.greymass.com" domain={PUBLIC_ANALYTICS_DOMAIN} />
-{/if}
