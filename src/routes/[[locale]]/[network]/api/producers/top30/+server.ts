72f35f99af91c7f631787fa86f4328243cc4cabf
aaroncox <aaron.cox@greymass.com>
2026-04-01 16:28:48 -0700

    Fixed issues with top 30 response and reduced caches

--- src/routes/[[locale]]/[network]/api/producers/top30/+server.ts
+++ src/routes/[[locale]]/[network]/api/producers/top30/+server.ts
@@ -7,11 +7,12 @@ import type { RequestEvent } from './$types';
 export async function GET({ locals: { network } }: RequestEvent) {
 	const all = await getProducersRecursive(network);
 	const producers = all
+		.filter((p) => p.is_active && Number(p.total_votes) > 0)
 		.sort((a, b) => Number(b.total_votes) - Number(a.total_votes))
 		.slice(0, 30)
 		.map((p) => p.owner);
 
-	const headers = getCacheHeaders(600);
+	const headers = getCacheHeaders(60);
 
 	return json(
 		{
